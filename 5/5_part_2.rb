ba2b625fc627ccf7b4a7084d1d21ea0efe0d82d2
Joergen Tjoennteig <jorgenlt@outlook.com>
2022-12-06 16:25:00 +0800

    dec 5th

--- 5/5_part_2.rb
+++ 5/5_part_2.rb
@@ -2,16 +2,16 @@ require "csv"
 filepath = "input.csv"
 
 CRATES = [
-  %w[R P C D B G],
-  %w[H V G],
-  %w[N S Q D J P M],
-  %w[P S L G D C N M],
-  %w[J B N C P F L S],
-  %w[Q B D Z V G T S],
-  %w[B Z M H F T Q],
-  %w[C M D B F],
-  %w[F C Q G],
-]
+          %w[R P C D B G],
+          %w[H V G],
+          %w[N S Q D J P M],
+          %w[P S L G D C N M],
+          %w[J B N C P F L S],
+          %w[Q B D Z V G T S],
+          %w[B Z M H F T Q],
+          %w[C M D B F],
+          %w[F C Q G],
+          ]
 
 # instructions as an array of strings, e.g. "move 2 from 2 to 3"
 instructions = []
@@ -22,15 +22,16 @@ end
 
 instructions.each do |instruction|
   moves = instruction.scan(/\d+/).map(&:to_i) # e.g. [2, 2, 3]
-
   number_of_moves = moves[0]
-  from = moves[1]
-  to = moves[2]
+  from_index = moves[1] - 1
+  to_index = moves[2] - 1
+  crate_length = CRATES[from_index].length
+  slice_from = (crate_length) - number_of_moves
 
-  number_of_moves. times do
-    CRATES[to - 1] << CRATES[from - 1].last
-    CRATES[from - 1].delete_at(-1)
-  end
+  CRATES[to_index] << CRATES[from_index].slice(slice_from, number_of_moves)
+  CRATES[to_index].flatten!
+
+  CRATES[from_index].pop(number_of_moves)
 end
 
 puts "Top of the stack:"
