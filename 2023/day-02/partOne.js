1df5f0b1ac45c1682dc859963e9c4b85772a1176
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2023-12-02 21:38:05 +0100

    day 2

--- 2023/day-02/partOne.js
+++ 2023/day-02/partOne.js
@@ -6,19 +6,28 @@ const sumPossibleGamesId = async () => {
 
     let sum = 0;
 
+    // Iterate over each game
     games.forEach((game) => {
+      // Extract the game ID from the game string
       const gameId = Number(game.match(/\d+/)[0]);
 
+      // Split the game string to get subsets of cubes
       const subsets = game.split(": ")[1].split("; ");
 
+      // Color variables
       let red = 0,
         green = 0,
         blue = 0;
 
+      // Iterate over each subset
       subsets.forEach((subset) => {
+        // Split the subset string to get individual cubes
         subset.split(",").forEach((cube) => {
+          // Extract the number of cubes and their color from the cube string
           const numberOfCubes = Number(cube.match(/\d+/)[0]);
           const color = cube.match(/[a-z]+/)[0];
+
+          // Check the color of the cube and update the corresponding color variable if necessary
           if (color === "red" && numberOfCubes > red) {
             red = numberOfCubes;
           }
@@ -31,8 +40,9 @@ const sumPossibleGamesId = async () => {
         });
       });
 
+      // If all color variables are within their limits, add the game ID to the sum
       if (red <= 12 && green <= 13 && blue <= 14) {
-        sum += gameId
+        sum += gameId;
       }
     });
 
--- 2023/day-02/partTwo.js
+++ 2023/day-02/partTwo.js
@@ -6,31 +6,40 @@ const sumPossibleGamesId = async () => {
 
     let sumOfPowersInSets = 0;
 
+    // Iterating over each game
     games.forEach((game) => {
-      const gameId = Number(game.match(/\d+/)[0]);
-
+      // Splitting the game into subsets
       const subsets = game.split(": ")[1].split("; ");
 
+      // Variables to store the number of red, green, and blue cubes
       let red = 0,
         green = 0,
         blue = 0;
 
+      // Iterating over each subset
       subsets.forEach((subset) => {
+        // Splitting the subset into cubes and iterating over each cube
         subset.split(",").forEach((cube) => {
+          // Extracting the number of cubes from the cube string
           const numberOfCubes = Number(cube.match(/\d+/)[0]);
+          // Extracting the color of the cubes from the cube string
           const color = cube.match(/[a-z]+/)[0];
+          // If the color is red and the number of cubes is greater than the current red count, update the red count
           if (color === "red" && numberOfCubes > red) {
             red = numberOfCubes;
           }
+          // If the color is green and the number of cubes is greater than the current green count, update the green count
           if (color === "green" && numberOfCubes > green) {
             green = numberOfCubes;
           }
+          // If the color is blue and the number of cubes is greater than the current blue count, update the blue count
           if (color === "blue" && numberOfCubes > blue) {
             blue = numberOfCubes;
           }
         });
       });
 
+      // Adding the product of red, green, and blue counts to the sum of powers in sets
       sumOfPowersInSets += red * green * blue;
     });
 
