f5d47536e5dd4d59b940e56c2e1335c82bd4d86d
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2023-12-04 18:43:17 +0100

    day 4, part 1

--- 2023/day-04/partOne.js
+++ 2023/day-04/partOne.js
@@ -3,10 +3,34 @@ import { readLines } from "../utils/readLines.js";
 const main = async () => {
   try {
     const input = await readLines("input.csv");
-    console.log(input);
+
+    totalPointsFromCards(input);
   } catch (err) {
     console.error(err);
   }
+};
+
+const totalPointsFromCards = (cards) => {
+  let totalPoints = 0;
+
+  cards.forEach((card) => {
+    const winningNumbers = new Set(
+      card.split("|")[0].trim().split(" ").slice(2).filter(Boolean).map(Number)
+    );
+    const cardNumbers = new Set(
+      card.split("|")[1].split(" ").slice(1).filter(Boolean).map(Number)
+    );
+    const commonNums = [...winningNumbers].filter((num) =>
+      cardNumbers.has(num)
+    );
+
+    if (commonNums.length === 1) {
+      totalPoints++;
+    } else if (commonNums.length > 1) {
+      totalPoints += 2 ** (commonNums.length - 1);
+    }
+  });
+  console.log(totalPoints);
 };
 
 // Call the main function
