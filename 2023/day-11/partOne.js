d30e52f38c5aaf9ddea7ee2f55153e49cb8b2bfd
Joergen Tjoennteig <jorgenlt@outlook.com>
2023-12-11 13:47:56 +0100

    save

--- 2023/day-11/partOne.js
+++ 2023/day-11/partOne.js
@@ -2,7 +2,12 @@ import { readFile } from "../utils/readFile.js";
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt");
+    let input = await readFile("input.txt");
+
+    let currentGalaxyNum = 1;
+    input = input.replace(/#/g, () => {
+      return currentGalaxyNum++;
+    });
 
     let rows = input.split("\n");
     const numRows = rows.length;
@@ -54,14 +59,89 @@ const main = async () => {
     // console.log("🚀 ~ main ~ rows:", rows.length);
     // console.log("🚀 ~ main ~ columns:", columns.length);
 
-    // change # to numbers
+    // Converting strings into arrays with single characters
+    rows = rows.map((row) =>
+      row.split("").map((e) => (isNaN(Number(e)) ? e : Number(e)))
+    );
+    columns = columns.map((column) =>
+      column.split("").map((e) => (isNaN(Number(e)) ? e : Number(e)))
+    );
+
+    // console.log("🚀 ~ main ~ rows:", rows);
+    // console.log("🚀 ~ main ~ columns:", columns);
+
+    // Changing #s to galaxy numbers
+    // let currentGalaxyNum = 1;
+
+    // rows = rows.map((row) => {
+    //   // console.log(row);
+    //   row = row.map((char) => {
+    //     // Assign the result of map function to row
+    //     if (char === "#") {
+    //       const galaxyNum = currentGalaxyNum;
+    //       currentGalaxyNum++;
+    //       return galaxyNum;
+    //     } else {
+    //       return char;
+    //     }
+    //   });
+    //   return row;
+    // });
+
+    // console.log("🚀 ~ main ~ rows:", rows);
+    // console.log("🚀 ~ main ~ columns:", columns);
 
     // Find pairs. Only count each pair once.
-
+    console.log(input)
+    console.log("currentGalaxyNum", currentGalaxyNum - 1)
+    const galaxyPairs = uniquePairs(currentGalaxyNum - 1);
+    // console.log("🚀 ~ main ~ galaxyPairs:", galaxyPairs)
+    // console.log("🚀 ~ main ~ galaxyPairs:", galaxyPairs.length)
 
     // For each pairs find the shortest path between them (the galxies)
+    // galaxyPairs.forEach((pair) => {
+    //   console.log("pair: ", pair);
+    //   const xyFrom = location(pair[0], rows, columns);
+    //   console.log("🚀 ~ galaxyPairs.forEach ~ xyFrom:", xyFrom);
+    //   const xyTo = location(pair[1], rows, columns);
+    //   console.log("🚀 ~ galaxyPairs.forEach ~ xyTo:", xyTo);
 
-    // console.log(input);
+    //   const distanceRows = subtractSmallest(xyFrom[0], xyTo[0]);
+    //   console.log("🚀 ~ galaxyPairs.forEach ~ distanceRows:", distanceRows)
+    //   const distanceColumns = subtractSmallest(xyFrom[1], xyTo[1]);
+    //   console.log("🚀 ~ galaxyPairs.forEach ~ distanceColumns:", distanceColumns)
+
+    //   const distance = distanceRows + distanceColumns;
+
+    //   console.log("distance:", distance);
+    // });
+    const distances = [];
+    // For each pairs find the shortest path between them (the galaxies)
+    galaxyPairs.forEach((pair) => {
+      // console.log("pair: ", pair);
+      const xyFrom = location(pair[0], rows, columns);
+      // console.log("🚀 ~ galaxyPairs.forEach ~ xyFrom:", xyFrom);
+      const xyTo = location(pair[1], rows, columns);
+      // console.log("🚀 ~ galaxyPairs.forEach ~ xyTo:", xyTo);
+
+      // Calculate Manhattan Distance
+      const distance =
+        Math.abs(xyFrom[0] - xyTo[0]) + Math.abs(xyFrom[1] - xyTo[1]);
+
+      distances.push(distance);
+      // console.log(
+      //   `Distance between galaxy ${pair[0]} and galaxy ${pair[1]}: ${distance}`
+      // );
+    });
+
+    const sumDistances = distances.reduce((prev, curr) => prev + curr);
+
+    console.log(sumDistances)
+    // Between galaxy 1 and galaxy 7: 15
+    // Between galaxy 3 and galaxy 6: 17
+    // Between galaxy 8 and galaxy 9: 5
+
+    // console.log(rows);
   } catch (err) {
     throw err;
   }
@@ -71,7 +151,8 @@ const main = async () => {
 const uniquePairs = (galaxies) => {
   const galaxiesArray = [];
 
-  for (let i = 1; i <= 9; i++) {
+  // Adjust here to stop at galaxies - 1
+  for (let i = 1; i <= galaxies; i++) {
     galaxiesArray.push(i);
   }
 
@@ -84,6 +165,46 @@ const uniquePairs = (galaxies) => {
   }
 
   return pairs;
+};
+
+
+// Function to find location of galaxy
+const location = (galaxyNum, rows, columns) => {
+  let x;
+  let y;
+
+  rows.forEach((row, i) => {
+    let index = row.indexOf(galaxyNum);
+    if (index !== -1) {
+      x = i;
+    }
+  });
+
+  columns.forEach((column, j) => {
+    let index = column.indexOf(galaxyNum);
+    if (index !== -1) {
+      y = j;
+    }
+  });
+
+  if (x === undefined || y === undefined) {
+    console.error(`Galaxy ${galaxyNum} not found. Rows and columns:`);
+    console.error(rows);
+    console.error(columns);
+    throw new Error(`Galaxy ${galaxyNum} not found`);
+  }
+
+  return [x, y];
+};
+
+
+
+// Function to subtract the smaller number from the larger
+const subtractSmallest = (x, y) => {
+  const min = Math.min(x, y);
+  const max = Math.max(x, y);
+
+  return max - min;
 };
 
 main();
