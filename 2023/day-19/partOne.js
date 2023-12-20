4d957f4a3854ce32a292f0a3557eeb1b295f2709
Joergen Tjoennteig <jorgenlt@outlook.com>
2023-12-20 17:46:07 +0100

    day 19 part 1

--- 2023/day-19/partOne.js
+++ 2023/day-19/partOne.js
@@ -1,59 +1,57 @@
 import fs from "fs/promises";
 
-const test = `
-px{a<2006:qkq,m>2090:A,rfg}
-pv{a>1716:R,A}
-lnx{m>1548:A,A}
-rfg{s<537:gd,x>2440:R,A}
-qs{s>3448:A,lnx}
-qkq{x<1416:A,crn}
-crn{x>2662:A,R}
-in{s<1351:px,qqz}
-qqz{s>2770:qs,m<1801:hdj,R}
-gd{a>3333:R,R}
-hdj{m>838:A,pv}
-
-{x=787,m=2655,a=1222,s=2876}
-{x=1679,m=44,a=2067,s=496}
-{x=2036,m=264,a=79,s=2244}
-{x=2461,m=1339,a=466,s=291}
-{x=2127,m=1623,a=2188,s=1013}
-`
-
+// Recursice function to check if a part is accepted
+// based on its workflow and conditions
 const isAccepted = (part, workflow, workflows) => {
-  console.log("workflow = ", workflow);
-  const conditions = workflows[workflow];
-  console.log("🚀 ~ isAccepted ~ conditions:", conditions);
-
+  // If the workflow is "A" or "R", the part is always accepted or rejected
   if (workflow === "A") return true;
   if (workflow === "R") return false;
 
+  // Get the conditions for the current workflow
+  const conditions = workflows[workflow];
+
+  // Loop through each condition
   for (let i = 0; i < conditions.length; i++) {
     const condition = conditions[i];
 
-    if (condition[1] === "<" || condition[1] === ">") {
-      const category = condition[0];
-      const value = parseInt(condition.match(/\d+/g));
-      const action = condition.split(":").at(-1);
-      console.log("🚀 ~ isAccepted ~ action:", action)
+    // If the condition is "A", the part is accepted
+    if (condition === "A") return true;
 
-      // if checking for Less Than
+    // If the condition is "R", the part is rejected
+    if (condition === "R") return false;
+
+    // If the condition is a valid workflow name,
+    // recursively check if the part is accepted for that workflow
+    if (condition in workflows) return isAccepted(part, condition, workflows);
+
+    // If the condition contains < or >
+    if (condition[1] === "<" || condition[1] === ">") {
+      // Get the category (x, m, a, s)
+      const category = condition[0];
+
+      // Get the value to compare against
+      const value = parseInt(condition.match(/\d+/g));
+
+      // Get the current action to execute if condition is true
+      const action = condition.split(":").at(-1);
+
+      // Get the next workflow to check if the condition false
+      const nextWorkflow = conditions[i + 1].split(":").at(-1);
+
+      // If checking for Less Than
       if (condition[1] === "<") {
-        // if statement is true, move on with action.
-        // else Continue loop
+        // If the condition is true, move on with the action
+        // Otherwise, continue to the next iteration of the loop
         if (part[category] < value) {
-          console.log("######")
-          console.log("isAccepted(part, action, workflows)", "=>", "isAccepted(", part, action, workflows, ")")
-          console.log("######")
           return isAccepted(part, action, workflows);
         } else {
           continue;
         }
-      
-      // if checking for Greater Than
+
+        // If checking for Greater Than
       } else if (condition[1] === ">") {
-        // if statement is true, move on with action
-        // else continue to next step
+        // If the condition is true, move on with the action
+        // Otherwise, continue to the next iteration of the loop
         if (part[category] > value) {
           return isAccepted(part, action, workflows);
         } else {
@@ -62,83 +60,64 @@ const isAccepted = (part, workflow, workflows) => {
       }
     }
 
-    continue
+    continue;
   }
 
-  console.log("no conditions match");
+  console.error("No conditions match");
   return null;
 };
 
 const main = async () => {
-  const input = (await fs.readFile("test.txt", "utf8")).trim().split("\n\n");
-  // const input = test.trim().split("\n\n");
-  // const workflows = input[0].split("\n");
+  const input = (await fs.readFile("input.txt", "utf8")).trim().split("\n\n");
 
-  // Creating an object with workflows and their conditions
-  // {
-  //   name: conditions
-  // }
-  //
-  // {
-  //   px: [ 'a<2006:qkq', 'm>2090:A', 'rfg' ],
-  //   pv: [ 'a>1716:R', 'A' ],
-  // }
+  // Object to store workflows and their conditions
   let workflows = {};
 
   input[0].split("\n").forEach((e) => {
+    // Get the workflow name
     const name = e.split("{")[0];
+
     const conditions = e
       .split("{")[1]
       .split(",")
       .map((c) => c.replace("}", ""));
-    // console.log(conditions)
-    // console.log(name)
 
+    // Add the workflow and its conditions to the workflows object
     workflows[name] = conditions;
   });
 
-  // Creating an array of parts with their properties
-  // parts:  [
-  //   { x: 653, m: 2123, a: 2908, s: 577 },
-  //   { x: 716, m: 172, a: 813, s: 2294 },
-  //   { x: 417, m: 2371, a: 1280, s: 962 },
-  //   { x: 1465, m: 1705, a: 1990, s: 994 },
+  // Array to store parts and their properties
+  let parts = [];
 
-  const parts = [];
-
+  // Populate parts array
   input[1]
     .split("\n")
     .map((e) => e.replace(/[{]|[}]/g, "").split(","))
     .forEach((part) => {
-      // console.log(part)
-
-      const x = parseInt(part[0].match(/\d+/)[0]);
-      const m = parseInt(part[1].match(/\d+/)[0]);
-      const a = parseInt(part[2].match(/\d+/)[0]);
-      const s = parseInt(part[3].match(/\d+/)[0]);
+      const x = parseInt(part[0].match(/\d+/)[0]); // Extract the value for x
+      const m = parseInt(part[1].match(/\d+/)[0]); // Extract the value for m
+      const a = parseInt(part[2].match(/\d+/)[0]); // Extract the value for a
+      const s = parseInt(part[3].match(/\d+/)[0]); // Extract the value for s
 
       parts.push({ x, m, a, s });
     });
 
+  // Set the initial workflow
   const initialWorkflow = "in";
-  
-  let sumAccepted = 0;
 
+  let sumAcceptedParts = 0;
+
+  // Loop through each part and check if it is accepted
   parts.forEach((part) => {
-    console.log("---");
-    console.log("checking part: ", part)
-    console.log(
-      "isAccepted(part, in, workflows): ",
-      isAccepted(part, "in", workflows)
-    );
-    console.log("---");
-    if (isAccepted(part, initialWorkflow, workflows)) {
-      sumAccepted += part.x + part.m + part.a + part.s;
+    const accepted = isAccepted(part, initialWorkflow, workflows);
+
+    // If the part is accepted, add its values to the sumAcceptedParts variable
+    if (accepted) {
+      sumAcceptedParts += part.x + part.m + part.a + part.s;
     }
-    // console.log(part)
   });
 
-  console.log(sumAccepted);
+  console.log("sumAcceptedParts:", sumAcceptedParts);
 };
 
 main();
