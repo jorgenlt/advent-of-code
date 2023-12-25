c2ade1568b67ae7dc0e72e92640297c1f1f920d4
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2023-12-25 14:01:31 +0100

    day 23 part 1 - changing dfs function, adding comments

--- 2023/day-23/partOne.js
+++ 2023/day-23/partOne.js
@@ -58,7 +58,7 @@ const main = async () => {
     // Define directions for each type of tile.
     const directions = {
       "^": [[-1, 0]],
-      "v": [[1, 0]],
+      v: [[1, 0]],
       ">": [[0, 1]],
       "<": [[0, -1]],
       ".": [
@@ -113,41 +113,75 @@ const main = async () => {
       }
     }
 
-    // Initialize a set to keep track of visited nodes.
-    let seen = new Set();
+    // Depth First Search, with detailed comments
 
-    // Initialize memoization table
+    // We start by creating a Set, `vistedNodes`. This will store all the nodes we've visited.
+    // Using a Set instead of an Array for this purpose is more efficient because checking 
+    // whether a value exists in a Set is generally faster than checking in an Array.
+    let visitedNodes = new Set();
+
+    // Next, we create a Map called `memo`. This will be our memoization table.
+    // Memoization is a technique where we store the results of expensive function calls 
+    // and reusing them when the same inputs occur again.
+    // In this case, we're storing the maximum score that we can get from each point in 
+    // the graph.
+    // Not really necessary in this puzzle, but it's good practice.
     const memo = new Map();
 
-    // Function to perform depth first search on the graph
-    const dfs = (point) => {
-      // Check if point has already been computed
-      if (memo.has(`${point[0]},${point[1]}`)) {
-        return memo.get(`${point[0]},${point[1]}`);
+    // Now we define our main function, depthFirstSearch. An algorithm used to 
+    // traverse trees or graphs.
+    // It starts at a given node, currentPoint, explores 
+    // as far as possible along each branch before backtracking.
+    const depthFirstSearch = (currentPoint) => {
+      // We convert the `currentPoint` array into a string so that it can be used as a key in our 
+      // `Set` and `Map`.
+      // This is necessary because JavaScript uses reference equality for arrays, not value 
+      // equality, meaning [1, 2] !== [1, 2].
+      const pointKey = `${currentPoint[0]},${currentPoint[1]}`;
+
+      // If we have already computed the result for this current point, we simply return it. 
+      // This is the essence of memoization.
+      if (memo.has(pointKey)) {
+        return memo.get(pointKey);
       }
 
-      // Base case: If the end tile is reached, return 0
-      if (point[0] === endTile[0] && point[1] === endTile[1]) {
+      // The base case for our recursive function: if the current point is the end 
+      // tile, we return 0.
+      // This signifies that we have found a valid path from the start to the end.
+      if (currentPoint[0] === endTile[0] && currentPoint[1] === endTile[1]) {
         return 0;
       }
 
-      let max = Number.NEGATIVE_INFINITY;
+      // We initialize `maxScore` to negative infinity. This variable will keep track of 
+      // the maximum score we can get from the current point.
+      let maxScore = Number.NEGATIVE_INFINITY;
 
-      seen.add(`${point[0]},${point[1]}`);
+      // We add the current point to our Set of visited nodes.
+      visitedNodes.add(pointKey);
 
-      for (let nx of graph[`${point[0]},${point[1]}`]) {
-        max = Math.max(max, dfs([nx[0], nx[1]]) + nx[2]);
+      // Now we loop over all neighbors of the current point in the graph.
+      for (let neighbor of graph[pointKey]) {
+        // For each neighbor, we recursively call `depthFirstSearch` function 
+        // and add the score of the current edge to the result.
+        // If this total is greater than our current `maxScore`, we update `maxScore`.
+        maxScore = Math.max(maxScore, depthFirstSearch([neighbor[0], neighbor[1]]) + neighbor[2]);
       }
 
-      seen.delete(`${point[0]},${point[1]}`);
+      // After exploring all neighbors, we remove the current point from our Set of 
+      // visited nodes.
+      visitedNodes.delete(pointKey);
 
-      // Store result in memoization table before returning
-      memo.set(`${point[0]},${point[1]}`, max);
+      // Before the function ends, we store the maximum score that we can get from the 
+      // current point in our memoization table.
+      // This way, if we encounter this point again in our search, we can simply look 
+      // up its score in the table rather than recomputing it.
+      memo.set(pointKey, maxScore);
 
-      return max;
+      // Return the maximum score.
+      return maxScore;
     };
 
-    console.log(dfs(startTile));
+    console.log(depthFirstSearch(startTile));
   } catch (err) {
     console.error(err);
   }
