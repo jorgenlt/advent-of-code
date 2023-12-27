2ed95c47686c11eaf84d5e037ea63a7d5b90d9b1
Joergen Tjoennteig <jorgenlt@outlook.com>
2023-12-27 17:22:42 +0100

    2015 - day 04 - part 1+2

---
+++ 2015/day-04/partTwo.js
@@ -0,0 +1,28 @@
+const crypto = require("crypto");
+
+const main = async () => {
+  try {
+    const input = "bgvyzdsv";
+
+    let string = input;
+    let i = 1;
+
+    while (true) {
+      const hash = crypto
+        .createHash("md5")
+        .update(string + i.toString())
+        .digest("hex");
+
+      if (hash.startsWith("000000")) {
+        console.log(input + i);
+        break;
+      }
+
+      i++;
+    }
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+main();
