83eec4dc5b1bde49d43939760d13f74ed11d12e9
Joergen Tjoennteig <jorgenlt@outlook.com>
2023-12-27 20:16:46 +0100

    2015 - day 05 - part 1+2

---
+++ 2015/day-05/partOne.js
@@ -0,0 +1,44 @@
+const { readFile } = require("node:fs/promises");
+
+const main = async () => {
+  try {
+    const input = (await readFile("input.txt", "utf8")).trim().split("\n");
+
+    const regexVowels = /[aeiou]/g;
+
+    const regexIllegalStrings = /(ab)|(cd)|(pq)|(xy)/g;
+
+    let niceCount = 0;
+
+    for (let i = 0; i < input.length; i++) {
+      const str = input[i];
+
+      if (regexIllegalStrings.test(str)) {
+        continue;
+      }
+
+      const vowels = str.match(regexVowels);
+
+      if (vowels && vowels.length >= 3) {
+        let prev = null;
+        for (let j = 0; j < str.length; j++) {
+          if (str[j] === prev) {
+            niceCount++;
+            break;
+          }
+          prev = str[j];
+
+          if (j === str.length - 1) {
+            prev = null;
+          }
+        }
+      }
+    }
+
+    console.log(niceCount);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+main();
---
+++ 2015/day-05/partTwo.js
@@ -0,0 +1,31 @@
+const { readFile } = require("node:fs/promises");
+
+const validateString = (str) => {
+  // Rule 1: Pair repeats
+  const hasPairRepeat = /([a-zA-Z]{2}).*\1/.test(str);
+
+  // Rule 2: Single letter repeat
+  const hasLetterRepeat = /([a-zA-Z]).{1}\1/.test(str);
+
+  return hasPairRepeat && hasLetterRepeat;
+};
+
+const main = async () => {
+  try {
+    const input = (await readFile("input.txt", "utf8")).trim().split("\n");
+
+    let niceCount = 0;
+
+    input.forEach((str) => {
+      if (validateString(str)) {
+        niceCount++;
+      }
+    });
+
+    console.log(niceCount);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+main();
