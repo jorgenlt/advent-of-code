7508c41b5a49babfd06318f7dcec8715aebe0ca4
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2023-12-30 17:50:26 +0100

    2017 day 1, part 1+2

--- 2017/day-01/partOne.js
+++ 2017/day-01/partOne.js
@@ -1,11 +1,31 @@
-  
 import { readFile } from "fs/promises";
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = (await readFile("input.txt", "utf-8"))
+      .trim()
+      .split("")
+      .map(Number);
+
+    let sum = 0;
+
+    for (let i = 0; i < input.length; i++) {
+      const num = input[i];
+
+      let nextNum;
+
+      if (i === input.length - 1) {
+        nextNum = input[0];
+      } else {
+        nextNum = input[i + 1];
+      }
+
+      if (num === nextNum) {
+        sum += num;
+      }
+    }
+
+    console.log(sum);
   } catch (err) {
     console.error(err);
   }
--- 2017/day-01/partTwo.js
+++ 2017/day-01/partTwo.js
@@ -1,11 +1,25 @@
-  
 import { readFile } from "fs/promises";
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = (await readFile("input2.txt", "utf-8"))
+      .trim()
+      .split("")
+      .map(Number);
+
+    let sum = 0;
+
+    for (let i = 0; i < (input.length / 2); i++) {
+      const num = input[i];
+
+      let nextNum = input[i + ((input.length) / 4)]
+
+      if (num === nextNum) {
+        sum += num;
+      }
+    }
+
+    console.log(sum);
   } catch (err) {
     console.error(err);
   }
