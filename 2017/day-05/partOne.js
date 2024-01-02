9a5bcb02fecd7edfbb0a679096eaa19247ac2551
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2024-01-02 11:12:58 +0100

    2017 day 5, part 1+2

--- 2017/day-05/partOne.js
+++ 2017/day-05/partOne.js
@@ -1,11 +1,28 @@
-  
 import { readFile } from "fs/promises";
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = (await readFile("input.txt", "utf-8"))
+      .trim()
+      .split("\n")
+      .map(Number);
+
+    const instructions = [...input];
+
+    let stepsCount = 0;
+    let offset = 0;
+
+    while (offset >= 0 && offset < instructions.length) {
+      const currentInstruction = instructions[offset];
+      
+      instructions[offset]++;
+      
+      offset = offset + currentInstruction;
+
+      stepsCount++;
+    }
+
+    console.log(stepsCount); //  354121
   } catch (err) {
     console.error(err);
   }
--- 2017/day-05/partTwo.js
+++ 2017/day-05/partTwo.js
@@ -1,11 +1,34 @@
-  
 import { readFile } from "fs/promises";
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = (await readFile("input.txt", "utf-8"))
+      .trim()
+      .split("\n")
+      .map(Number);
+
+    const instructions = [...input];
+
+    let stepsCount = 0;
+    let offset = 0;
+
+    while (offset >= 0 && offset < instructions.length) {
+      // Save current offset in a variable
+      const currentOffset = offset;
+
+      // Add instruction at offset to offset.
+      offset += instructions[offset];
+
+      if (instructions[currentOffset] >= 3) {
+        instructions[currentOffset] += -1;
+      } else {
+        instructions[currentOffset] += 1;
+      }
+
+      stepsCount++;
+    }
+
+    console.log(stepsCount); //  27283023
   } catch (err) {
     console.error(err);
   }
