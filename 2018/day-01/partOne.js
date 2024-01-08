26e0117070b9961f834fafcb4ef790cfa0a01d1f
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2024-01-08 22:03:17 +0100

    2018 day 1, part 1 + 2

--- 2018/day-01/partOne.js
+++ 2018/day-01/partOne.js
@@ -1,11 +1,15 @@
-  
 import { readFile } from "fs/promises";
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = (await readFile("input.txt", "utf-8"))
+      .trim()
+      .split("\n")
+      .map(Number);
+
+    const currentFreq = input.reduce((acc, num) => acc + num);
+
+    console.log(currentFreq);
   } catch (err) {
     console.error(err);
   }
--- 2018/day-01/partTwo.js
+++ 2018/day-01/partTwo.js
@@ -1,11 +1,34 @@
-  
 import { readFile } from "fs/promises";
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
+    const input = (await readFile("input.txt", "utf-8"))
+      .trim()
+      .split("\n")
+      .map(Number);
+
+    // Storing unique values in a Set
+    const freqs = new Set();
     
-    console.log(input);
+    let currentFreq = 0;
+
+    let i = 0;
+    while (true) {
+      currentFreq += input[i];
+
+      if (freqs.has(currentFreq)) {
+        break;
+      }
+
+      freqs.add(currentFreq);
+
+      // If i is last index, set to 0. Else iterate by 1.
+      i === input.length - 1 ? (i = 0) : i++;
+    }
+
+    console.log(currentFreq);
+
+    return;
   } catch (err) {
     console.error(err);
   }
