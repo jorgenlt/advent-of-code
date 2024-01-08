233d2625df8b85e01ec25b257c478ca30687ba10
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2024-01-08 22:48:59 +0100

    2018 day 2, part 1 + 2

--- 2018/day-02/partOne.js
+++ 2018/day-02/partOne.js
@@ -1,11 +1,42 @@
-  
 import { readFile } from "fs/promises";
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = (await readFile("input.txt", "utf-8")).trim().split("\n");
+
+    let twoLetterCount = 0;
+    let threeLetterCount = 0;
+
+    input.forEach((line) => {
+      const map = new Map();
+
+      line.split("").forEach((letter) => {
+        if (map.has(letter)) {
+          map.set(letter, map.get(letter) + 1);
+        } else {
+          map.set(letter, 1);
+        }
+      });
+
+      let foundTwo = false;
+      let foundThree = false;
+
+      map.forEach((value, key) => {
+        if (value === 2 && foundTwo === false) {
+          foundTwo = true;
+          twoLetterCount++;
+        }
+
+        if (value === 3 && foundThree === false) {
+          foundThree = true;
+          threeLetterCount++;
+        }
+      });
+    });
+
+    const checkSum = twoLetterCount * threeLetterCount;
+
+    console.log(checkSum);
   } catch (err) {
     console.error(err);
   }
--- 2018/day-02/partTwo.js
+++ 2018/day-02/partTwo.js
@@ -1,11 +1,36 @@
-  
 import { readFile } from "fs/promises";
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = (await readFile("input.txt", "utf-8")).trim().split("\n");
+
+    for (let i = 0; i < input.length; i++) {
+      const id1 = input[i];
+
+      for (let j = i + 1; j < input.length; j++) {
+        const id2 = input[j];
+
+        let diffCount = 0;
+
+        for (let k = 0; k < id1.length; k++) {
+          if (id1[k] !== id2[k]) {
+            diffCount++;
+          }
+        }
+
+        if (diffCount === 1) {
+          let commonLetters = "";
+          for (let l = 0; l < id1.length; l++) {
+            if (id1[l] === id2[l]) {
+              commonLetters += id1[l];
+            }
+          }
+
+          console.log(commonLetters);
+          return;
+        }
+      }
+    }
   } catch (err) {
     console.error(err);
   }
