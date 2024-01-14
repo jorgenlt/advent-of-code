33edc4e97c79bb41040585dfbd994605bdf512da
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2024-01-14 10:06:07 +0530

    2019, day 1, part 1+2

--- 2019/day-01/partOne.js
+++ 2019/day-01/partOne.js
@@ -1,11 +1,21 @@
-  
 import { readFile } from "fs/promises";
+
+const calcFuel = (mass) => Math.floor(mass / 3) - 2;
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = (await readFile("input.txt", "utf-8"))
+      .trim()
+      .split("\n")
+      .map(Number);
+
+    let fuelCounter = 0;
+
+    input.forEach((mass) => {
+      fuelCounter += calcFuel(mass);
+    });
+
+    console.log(fuelCounter);
   } catch (err) {
     console.error(err);
   }
--- 2019/day-01/partTwo.js
+++ 2019/day-01/partTwo.js
@@ -1,11 +1,29 @@
-  
 import { readFile } from "fs/promises";
+
+const calcFuel = (mass) => Math.floor(mass / 3) - 2;
+
+const recursiveCalcFuel = (mass) => {
+  const fuel = calcFuel(mass);
+
+  if (fuel <= 0) {
+    return 0;
+  } else {
+    return fuel + recursiveCalcFuel(fuel);
+  }
+};
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = (await readFile("input.txt", "utf-8"))
+      .trim()
+      .split("\n")
+      .map(Number);
+
+    let recursiveFuelCounter = 0;
+
+    input.forEach((mass) => recursiveFuelCounter += recursiveCalcFuel(mass));
+
+    console.log(recursiveFuelCounter);
   } catch (err) {
     console.error(err);
   }
