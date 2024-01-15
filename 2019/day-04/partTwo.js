f2d736a0da30f639dcd803c11062b6d0ffd16ee2
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2024-01-15 20:22:12 +0530

    2019, day 4, part 1 + 2

--- 2019/day-04/partTwo.js
+++ 2019/day-04/partTwo.js
@@ -1,11 +1,32 @@
-  
-import { readFile } from "fs/promises";
+import hasStrictlyTwoAdjacentDuplicates from "../../utils/hasStrictlyTwoAdjacentDuplicates.js";
 
-const main = async () => {
+const doesNeverDecrease = (str) => {
+  for (let i = 0; i < str.length; i++) {
+    if (str[i + 1] < str[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const main = () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = "197487-673251".split("-").map(Number);
+
+    const [from, to] = input;
+
+    const passwords = [];
+
+    for (let i = from; i <= to; i++) {
+      if (
+        hasStrictlyTwoAdjacentDuplicates(i.toString()) &&
+        doesNeverDecrease(i.toString())
+      ) {
+        passwords.push(i);
+      }
+    }
+
+    console.log(passwords.length); // 1126
   } catch (err) {
     console.error(err);
   }
