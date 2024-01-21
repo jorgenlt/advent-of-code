e4264afaf496e3632db25d20e59806ec5de1adbb
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2024-01-21 20:35:07 +0530

    2020 day 2 part 1 + 2

--- 2020/day-02/partOne.js
+++ 2020/day-02/partOne.js
@@ -1,11 +1,26 @@
-  
 import { readFile } from "fs/promises";
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = (await readFile("input.txt", "utf-8")).trim().split("\n");
+
+    let validCount = 0;
+
+    input.forEach((pwList) => {
+      const [_, least, most, letter, password] = pwList.match(
+        /(\d+)-(\d+) (\w): (\w+)/
+      );
+
+      const letterRepeats = password
+        .split("")
+        .filter((e) => e === letter).length;
+
+      if (letterRepeats >= least && letterRepeats <= most) {
+        validCount++;
+      }
+    });
+
+    console.log(validCount);
   } catch (err) {
     console.error(err);
   }
--- 2020/day-02/partTwo.js
+++ 2020/day-02/partTwo.js
@@ -1,11 +1,25 @@
-  
 import { readFile } from "fs/promises";
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = (await readFile("input.txt", "utf-8")).trim().split("\n");
+
+    let validCount = 0;
+
+    input.forEach((pwList) => {
+      const [_, pos1, pos2, letter, password] = pwList.match(
+        /(\d+)-(\d+) (\w): (\w+)/
+      );
+
+      const isLetterPos1 = password[pos1 - 1] === letter;
+      const isLetterPos2 = password[pos2 - 1] === letter;
+
+      if ((isLetterPos1 && !isLetterPos2) || (!isLetterPos1 && isLetterPos2)) {
+        validCount++;
+      }
+    });
+
+    console.log(validCount);
   } catch (err) {
     console.error(err);
   }
