65248123eac5c898b0fe99e196dd179798f76dd3
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2024-01-21 18:54:00 +0530

    2020 part 1 + 2

--- 2020/day-01/partOne.js
+++ 2020/day-01/partOne.js
@@ -1,11 +1,25 @@
-  
 import { readFile } from "fs/promises";
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = (await readFile("input.txt", "utf-8"))
+      .trim()
+      .split("\n")
+      .map(Number);
+
+    for (let i = 0; i < input.length - 1; i++) {
+      const num1 = input[i];
+
+      for (let j = 1; j < input.length; j++) {
+        const num2 = input[j];
+        const sum = num1 + num2;
+
+        if (sum === 2020) {
+          console.log(num1 * num2);
+          return;
+        }
+      }
+    }
   } catch (err) {
     console.error(err);
   }
--- 2020/day-01/partTwo.js
+++ 2020/day-01/partTwo.js
@@ -1,11 +1,30 @@
-  
 import { readFile } from "fs/promises";
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = (await readFile("input.txt", "utf-8"))
+      .trim()
+      .split("\n")
+      .map(Number);
+
+    for (let i = 0; i < input.length - 2; i++) {
+      const num1 = input[i];
+
+      for (let j = 1; j < input.length - 1; j++) {
+        const num2 = input[j];
+
+        for (let k = 2; k < input.length; k++) {
+          const num3 = input[k];
+          
+          const sum = num1 + num2 + num3;
+
+          if (sum === 2020) {
+            console.log(num1 * num2 * num3);
+            return;
+          }
+        }
+      }
+    }
   } catch (err) {
     console.error(err);
   }
