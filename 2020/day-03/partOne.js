e63e93855e347527408a40b6eb6e1033c7a4a1da
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2024-01-22 18:55:40 +0530

    2020 day 3 part 1 + 2

--- 2020/day-03/partOne.js
+++ 2020/day-03/partOne.js
@@ -1,11 +1,52 @@
-  
 import { readFile } from "fs/promises";
+
+const extendInput = (input, multiplier) => {
+  const newInput = [...input];
+
+  for (let i = 0; i < multiplier; i++) {
+    for (let j = 0; j < input.length; j++) {
+      newInput[j].push(...input[j]);
+    }
+  }
+
+  return newInput;
+};
+
+const isTree = (input, coords) => {
+  const [row, col] = coords;
+
+  if (input[row][col] === "#") {
+    return true;
+  }
+
+  return false;
+};
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = (await readFile("input.txt", "utf-8"))
+      .trim()
+      .split("\n")
+      .map((e) => e.split(""));
+
+    const extended = extendInput(input, 7); // Extend until treeCount stays the same as last multiplier
+
+    let treeCount = 0;
+
+    const direction = [1, 3]; // [row, col]
+
+    let currentCoords = [...direction];
+
+    while (currentCoords[0] < extended.length) {
+      if (isTree(extended, currentCoords)) {
+        treeCount++;
+      }
+
+      currentCoords[0] += direction[0];
+      currentCoords[1] += direction[1];
+    }
+
+    console.log(treeCount);
   } catch (err) {
     console.error(err);
   }
--- 2020/day-03/partTwo.js
+++ 2020/day-03/partTwo.js
@@ -1,11 +1,67 @@
-  
 import { readFile } from "fs/promises";
+
+const extendInput = (input, multiplier) => {
+  const newInput = [...input];
+
+  for (let i = 0; i < multiplier; i++) {
+    for (let j = 0; j < input.length; j++) {
+      newInput[j].push(...input[j]);
+    }
+  }
+
+  return newInput;
+};
+
+const isTree = (input, coords) => {
+  const [row, col] = coords;
+
+  if (input[row][col] === "#") {
+    return true;
+  }
+
+  return false;
+};
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = (await readFile("input.txt", "utf-8"))
+      .trim()
+      .split("\n")
+      .map((e) => e.split(""));
+
+    const extended = extendInput(input, 7); // Use multiplier from part 1
+
+    let treeCounts = [];
+
+    // [row, col]
+    const directions = [
+      [1, 1],
+      [1, 3],
+      [1, 5],
+      [1, 7],
+      [2, 1],
+    ];
+
+    directions.forEach((direction) => {
+      let treeCount = 0;
+
+      let currentCoords = [...direction];
+
+      while (currentCoords[0] < extended.length) {
+        if (isTree(extended, currentCoords)) {
+          treeCount++;
+        }
+
+        currentCoords[0] += direction[0];
+        currentCoords[1] += direction[1];
+      }
+
+      treeCounts.push(treeCount);
+    });
+
+    const result = treeCounts.reduce((acc, curr) => acc * curr);
+
+    console.log(result);
   } catch (err) {
     console.error(err);
   }
