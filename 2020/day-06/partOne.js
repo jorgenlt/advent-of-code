5f951de2e9dd22cb4f6ddbfb550f2f105a508fbe
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2024-01-23 19:56:50 +0530

    2020 day 6 part 1 + 2

--- 2020/day-06/partOne.js
+++ 2020/day-06/partOne.js
@@ -1,11 +1,29 @@
-  
 import { readFile } from "fs/promises";
+
+const countUniqueLetters = (str) => {
+  const uniqueLetters = new Set(str);
+  return uniqueLetters.size;
+};
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = (await readFile("input.txt", "utf-8"))
+      .trim()
+      .split("\n\n")
+      .map((e) => {
+        return e.split("\n");
+      });
+
+    let questionCount = 0;
+
+    input.forEach((group) => {
+      const groupStr = group.join("");
+      const uniqueQuestions = countUniqueLetters(groupStr);
+
+      questionCount += uniqueQuestions;
+    });
+
+    console.log(questionCount);
   } catch (err) {
     console.error(err);
   }
--- 2020/day-06/partTwo.js
+++ 2020/day-06/partTwo.js
@@ -1,11 +1,36 @@
-  
 import { readFile } from "fs/promises";
+
+const countCommonLetters = (arr) => {
+  if (arr.length === 1) {
+    return arr[0].length;
+  }
+
+  const commonLetters = arr.reduce(
+    (acc, currentString) => {
+      const currentSet = new Set(currentString);
+
+      return acc.filter((letter) => currentSet.has(letter));
+    },
+    [...new Set(arr[0])]
+  );
+
+  return commonLetters.length;
+};
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = (await readFile("input.txt", "utf-8"))
+      .trim()
+      .split("\n\n")
+      .map((e) => {
+        return e.split("\n");
+      });
+
+    let questionCount = 0;
+
+    input.forEach((group) => (questionCount += countCommonLetters(group)));
+
+    console.log(questionCount);
   } catch (err) {
     console.error(err);
   }
