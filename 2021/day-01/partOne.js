0a280ae8c28de02cbece8894c6c9f9819fe9ddc4
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2024-01-25 19:05:31 +0530

    2021 day 1 part 1

--- 2021/day-01/partOne.js
+++ 2021/day-01/partOne.js
@@ -1,11 +1,24 @@
-  
 import { readFile } from "fs/promises";
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = (await readFile("input.txt", "utf-8"))
+      .trim()
+      .split("\n")
+      .map(Number);
+
+    let result = 0;
+    let prevDepth = input[0];
+
+    for (let i = 1; i < input.length; i++) {
+      const currentDepth = input[i];
+      if (currentDepth > prevDepth) {
+        result++;
+      }
+      prevDepth = currentDepth;
+    }
+
+    console.log(result);
   } catch (err) {
     console.error(err);
   }
--- 2021/day-01/partTwo.js
+++ 2021/day-01/partTwo.js
@@ -1,11 +1,28 @@
-  
 import { readFile } from "fs/promises";
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = (await readFile("input.txt", "utf-8"))
+      .trim()
+      .split("\n")
+      .map(Number);
+
+    let result = 0;
+
+    for (let i = 3; i < input.length; i++) {
+      const num1 = input[i - 3];
+      const num2 = input[i - 2];
+      const num3 = input[i - 1];
+      const num4 = input[i];
+      const sumFirstWindow = num1 + num2 + num3;
+      const sumSecondWindow = num2 + num3 + num4;
+
+      if (sumSecondWindow > sumFirstWindow) {
+        result++;
+      }
+    }
+
+    console.log(result);
   } catch (err) {
     console.error(err);
   }
