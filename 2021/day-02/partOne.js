958e2470c43d1e9d9b94ef5a8f28e03ba876191b
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2024-01-25 19:42:31 +0530

    2021 day 2 part 1 + 2

--- 2021/day-02/partOne.js
+++ 2021/day-02/partOne.js
@@ -1,11 +1,38 @@
-  
 import { readFile } from "fs/promises";
+
+const move = (dir, length) => {
+  const moves = {
+    forward: [length, 0],
+    down: [0, length],
+    up: [0, -length],
+  };
+
+  return moves[dir] || console.error("Dir not found.");
+};
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = (await readFile("input.txt", "utf-8"))
+      .trim()
+      .split("\n")
+      .map((e) => {
+        const [dir, length] = e.split(" ");
+        return [dir, Number(length)];
+      });
+
+    let currPos = [0, 0];
+
+    input.forEach((step) => {
+      const [dir, length] = step;
+
+      const [moveX, moveY] = move(dir, length);
+
+      const [currX, currY] = currPos;
+
+      currPos = [currX + moveX, currY + moveY];
+    });
+
+    console.log(currPos[0] * currPos[1]);
   } catch (err) {
     console.error(err);
   }
--- 2021/day-02/partTwo.js
+++ 2021/day-02/partTwo.js
@@ -1,11 +1,40 @@
-  
 import { readFile } from "fs/promises";
+
+const move = (dir, length, aim) => {
+  const moves = {
+    forward: [length, length * aim, 0],
+    down: [0, 0, length],
+    up: [0, 0, -length],
+  };
+
+  return moves[dir] || console.error("Dir not found.");
+};
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = (await readFile("input.txt", "utf-8"))
+      .trim()
+      .split("\n")
+      .map((e) => {
+        const [dir, length] = e.split(" ");
+        return [dir, Number(length)];
+      });
+
+    let currPos = [0, 0, 0]; // [x, y, aim]
+
+    input.forEach((step) => {
+      const [dir, length] = step;
+
+      const [currX, currY, currAim] = currPos;
+
+      const [moveX, moveY, aim] = move(dir, length, currAim);
+
+      currPos = [currX + moveX, currY + moveY, currAim + aim];
+    });
+
+    const [x, y] = currPos;
+
+    console.log(x * y);
   } catch (err) {
     console.error(err);
   }
