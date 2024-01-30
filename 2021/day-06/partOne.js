faac89eabaf5e63d61a09f2bbbd95ce1319422bc
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2024-01-30 20:47:20 +0530

    2021 day 6 part 1 + 2

--- 2021/day-06/partOne.js
+++ 2021/day-06/partOne.js
@@ -1,11 +1,34 @@
-  
 import { readFile } from "fs/promises";
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = (await readFile("input.txt", "utf-8"))
+      .trim()
+      .split(",")
+      .map(Number);
+
+    let prevFish = [...input];
+    let currentFish = [];
+
+    for (let i = 0; i < 80; i++) {
+      currentFish = [...prevFish];
+
+      // How many lanternfish would there be after 80 days?
+      prevFish.forEach((fish, j) => {
+        // If 0, a new fish is created and set to 8 days. Reset fish to 6.
+        if (fish === 0) {
+          currentFish[j] = 6;
+          currentFish.push(8);
+        } else {
+          // Current fish has 1 day less till cycle end
+          currentFish[j] = currentFish[j] - 1;
+        }
+      });
+
+      prevFish = [...currentFish];
+    }
+
+    console.log(currentFish.length);
   } catch (err) {
     console.error(err);
   }
--- 2021/day-06/partTwo.js
+++ 2021/day-06/partTwo.js
@@ -1,11 +1,24 @@
-  
 import { readFile } from "fs/promises";
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const counts = Array(9).fill(0);
+
+    (await readFile("input.txt", "utf-8"))
+      .trim()
+      .split(",")
+      .map(Number)
+      .forEach((e) => (counts[e] += 1));
+
+    for (let i = 0; i < 256; i++) {
+      const newCount = counts.shift();
+      counts[6] += newCount;
+      counts.push(newCount);
+    }
+
+    const result = counts.reduce((prev, curr) => prev + curr, 0);
+
+    console.log(result);
   } catch (err) {
     console.error(err);
   }
