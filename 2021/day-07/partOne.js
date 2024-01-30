b5dc5dd524cb570dcf71084b433df867ad177ce9
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2024-01-31 05:19:22 +0530

    2021 day 7 part 1 + 2

--- 2021/day-07/partOne.js
+++ 2021/day-07/partOne.js
@@ -1,11 +1,39 @@
-  
 import { readFile } from "fs/promises";
+
+const fuelCost = (fromPos, toPos) => {
+  return Math.abs(fromPos - toPos);
+};
+
+const leastFuel = (positions) => {
+  const posStart = Math.min(...positions);
+  const posEnd = Math.max(...positions);
+
+  let leastFuel = Number.MAX_VALUE;
+
+  for (let i = posStart; i <= posEnd; i++) {
+    // find fuelcost for each position to move to position i
+    let fuelCostCount = 0;
+
+    positions.forEach((pos) => {
+      fuelCostCount += fuelCost(pos, i);
+    });
+
+    if (fuelCostCount < leastFuel) {
+      leastFuel = fuelCostCount;
+    }
+  }
+
+  return leastFuel;
+};
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const positions = (await readFile("input.txt", "utf-8"))
+      .trim()
+      .split(",")
+      .map(Number);
+
+    console.log(leastFuel(positions));
   } catch (err) {
     console.error(err);
   }
--- 2021/day-07/partTwo.js
+++ 2021/day-07/partTwo.js
@@ -1,11 +1,51 @@
-  
 import { readFile } from "fs/promises";
+
+const fuelCost = (fromPos, toPos) => {
+  const steps = Math.abs(fromPos - toPos);
+
+  let fuelCostCount = 0;
+
+  let prevCost = 0;
+
+  for (let i = 0; i < steps; i++) {
+    fuelCostCount += prevCost + 1;
+
+    prevCost++;
+  }
+
+  return fuelCostCount;
+};
+
+const leastFuel = (positions) => {
+  const posStart = Math.min(...positions);
+  const posEnd = Math.max(...positions);
+
+  let leastFuel = Number.MAX_VALUE;
+
+  for (let i = posStart; i <= posEnd; i++) {
+    // find fuelcost for each position to move to position i
+    let fuelCostCount = 0;
+
+    positions.forEach((pos) => {
+      fuelCostCount += fuelCost(pos, i);
+    });
+
+    if (fuelCostCount < leastFuel) {
+      leastFuel = fuelCostCount;
+    }
+  }
+
+  return leastFuel;
+};
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const positions = (await readFile("input.txt", "utf-8"))
+      .trim()
+      .split(",")
+      .map(Number);
+
+    console.log(leastFuel(positions));
   } catch (err) {
     console.error(err);
   }
