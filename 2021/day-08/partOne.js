7c92ffd5ea8ffd19aebcd505be6949701f3ac558
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2024-02-02 16:46:56 +0700

    2021 day 8 part 1

--- 2021/day-08/partOne.js
+++ 2021/day-08/partOne.js
@@ -1,11 +1,21 @@
-  
 import { readFile } from "fs/promises";
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = (await readFile("input.txt", "utf-8"))
+      .trim()
+      .split("\n")
+      .map((e) => e.split(" | "))
+      .map((e) => [e[0].split(" "), e[1].split(" ")]);
+
+    const result = input.reduce(
+      (acc, curr) =>
+        acc +
+        curr[1].filter((str) => str.length <= 4 || str.length === 7).length,
+      0
+    );
+
+    console.log(result);
   } catch (err) {
     console.error(err);
   }
