a058fd89ca204a8ed564d9b894ea6835125e14b3
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2024-02-05 10:20:28 +0800

    2022 day 1 part 1 + 2

--- 2022/day-01/partOne.js
+++ 2022/day-01/partOne.js
@@ -1,11 +1,17 @@
-  
 import { readFile } from "fs/promises";
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = (await readFile("input.txt", "utf-8"))
+      .trim()
+      .split("\n\n")
+      .map((e) => e.split("\n").map(Number));
+
+    const mostCals = Math.max(
+      ...input.map((elf) => elf.reduce((acc, curr) => acc + curr, 0))
+    );
+
+    console.log(mostCals);
   } catch (err) {
     console.error(err);
   }
--- 2022/day-01/partTwo.js
+++ 2022/day-01/partTwo.js
@@ -1,11 +1,23 @@
-  
 import { readFile } from "fs/promises";
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = (await readFile("input.txt", "utf-8"))
+      .trim()
+      .split("\n\n")
+      .map((e) => e.split("\n").map(Number));
+
+    const calories = [];
+
+    input.forEach((elf) => {
+      calories.push(elf.reduce((acc, curr) => acc + curr, 0));
+    });
+
+    calories.sort((a, b) => b - a);
+
+    const [elf1, elf2, elf3] = calories;
+
+    console.log(elf1 + elf2 + elf3);
   } catch (err) {
     console.error(err);
   }
