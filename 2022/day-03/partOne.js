61af3141abf1dd4405bbfc1b6b86ef473ecc3a2e
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2024-02-06 18:37:36 +0800

    2022 day 3 part 1 + 2

--- 2022/day-03/partOne.js
+++ 2022/day-03/partOne.js
@@ -1,11 +1,38 @@
-  
 import { readFile } from "fs/promises";
+import createLetterNumberMapping from "../../utils/createLetterNumberMapping.js";
+
+const PRIORITIES = createLetterNumberMapping();
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const rucksacks = (await readFile("input.txt", "utf-8"))
+      .trim()
+      .split("\n")
+      .map((rucksack) => {
+        rucksack = rucksack.split("");
+        const compartment1 = rucksack.slice(0, rucksack.length / 2);
+        const compartment2 = rucksack.slice(
+          rucksack.length / 2,
+          rucksack.length
+        );
+        const commonItems = compartment1.filter((item) =>
+          compartment2.includes(item)
+        );
+
+        return {
+          compartment1,
+          compartment2,
+          commonItems,
+        };
+      });
+
+    let prioritiesCount = 0;
+
+    rucksacks.forEach((rucksack) => {
+      prioritiesCount += PRIORITIES[rucksack.commonItems[0]];
+    });
+
+    console.log(prioritiesCount);
   } catch (err) {
     console.error(err);
   }
--- 2022/day-03/partTwo.js
+++ 2022/day-03/partTwo.js
@@ -1,11 +1,30 @@
-  
 import { readFile } from "fs/promises";
+import createLetterNumberMapping from "../../utils/createLetterNumberMapping.js";
+import findCommonItems from "../../utils/findCommonItems.js";
+
+const PRIORITIES = createLetterNumberMapping();
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const rucksacks = (await readFile("input.txt", "utf-8"))
+      .trim()
+      .split("\n")
+      .map((rucksack) => rucksack.split(""));
+    const groups = [];
+
+    for (let i = 0; i < rucksacks.length; i += 3) {
+      const group = [rucksacks[i], rucksacks[i + 1], rucksacks[i + 2]];
+      groups.push(group);
+    }
+
+    let prioritiesCount = 0;
+
+    groups.forEach((group) => {
+      const common = findCommonItems(...group)[0];
+      prioritiesCount += PRIORITIES[common];
+    });
+
+    console.log(prioritiesCount);
   } catch (err) {
     console.error(err);
   }
