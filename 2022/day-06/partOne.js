97f0d25edab40d4d17a086efd7852bb7123c40bf
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2024-02-08 16:47:04 +1300

    2022 day 6 part 1 + 2

--- 2022/day-06/partOne.js
+++ 2022/day-06/partOne.js
@@ -1,11 +1,21 @@
-  
 import { readFile } from "fs/promises";
+
+const areAllItemsUnique = (arr) => {
+  return arr.length === new Set(arr).size;
+};
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const buffer = (await readFile("input.txt", "utf-8")).trim().split("");
+
+    for (let i = 3; i < buffer.length; i++) {
+      const chars = buffer.slice(i - 3, i + 1);
+
+      if (areAllItemsUnique(chars)) {
+        console.log(i + 1);
+        return;
+      }
+    }
   } catch (err) {
     console.error(err);
   }
