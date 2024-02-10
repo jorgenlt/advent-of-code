14cea6cc1b615ba474e5b41e81aff591ca3cf98f
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2024-02-10 16:55:33 +0800

    2022 day 8 part 1 + 2

--- 2022/day-08/partOne.js
+++ 2022/day-08/partOne.js
@@ -1,11 +1,44 @@
-  
 import { readFile } from "fs/promises";
+import transposeMatrix from "../../utils/transposeMatrix.js";
+
+const isLarger = (number, array) => array.every((elem) => number > elem);
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const forest = (await readFile("input.txt", "utf-8"))
+      .trim()
+      .split("\n")
+      .map((e) => e.split("").map(Number));
+
+    const transposedForest = transposeMatrix(forest);
+
+    // For each tree. Find every tree left, right, up, down
+    // Skip checking the edges. They are already visible (i=1, j=1)
+
+    let visibleCount = 0;
+
+    for (let i = 1; i < forest.length - 1; i++) {
+      for (let j = 1; j < forest[i].length - 1; j++) {
+        const tree = forest[i][j];
+        const treesRow = forest[i];
+        const treesCol = transposedForest[j];
+
+        // Check all directions if tree is the highest
+        const left = isLarger(tree, treesRow.slice(0, j));
+        const right = isLarger(tree, treesRow.slice(j + 1, treesRow.length));
+        const up = isLarger(tree, treesCol.slice(0, i));
+        const down = isLarger(tree, treesCol.slice(i + 1, treesRow.length));
+
+        if (left || right || up || down) {
+          visibleCount++;
+        }
+      }
+    }
+
+    // Add the trees on the edge to visibleCount
+    visibleCount += 2 * forest.length + 2 * transposedForest.length - 4;
+
+    console.log("Visible:", visibleCount);
   } catch (err) {
     console.error(err);
   }
--- 2022/day-08/partTwo.js
+++ 2022/day-08/partTwo.js
@@ -1,11 +1,60 @@
-  
 import { readFile } from "fs/promises";
+import transposeMatrix from "../../utils/transposeMatrix.js";
+
+const calcViewingDist = (tree, trees) => {
+  let viewingDistance = 0;
+
+  for (let i = 0; i < trees.length; i++) {
+    viewingDistance++;
+
+    if (trees[i] >= tree) {
+      return viewingDistance;
+    }
+  }
+
+  return viewingDistance;
+};
+
+const calcScenicScore = (tree, left, right, up, down) => {
+  return (
+    calcViewingDist(tree, left) *
+    calcViewingDist(tree, right) *
+    calcViewingDist(tree, up) *
+    calcViewingDist(tree, down)
+  );
+};
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const forest = (await readFile("input.txt", "utf-8"))
+      .trim()
+      .split("\n")
+      .map((e) => e.split("").map(Number));
+
+    const transposedForest = transposeMatrix(forest);
+
+    let highestScenicScore = 0;
+
+    for (let i = 1; i < forest.length - 1; i++) {
+      for (let j = 1; j < forest[i].length - 1; j++) {
+        const tree = forest[i][j];
+        const treesRow = forest[i];
+        const treesCol = transposedForest[j];
+
+        const left = treesRow.slice(0, j).reverse();
+        const right = treesRow.slice(j + 1, treesRow.length);
+        const up = treesCol.slice(0, i).reverse();
+        const down = treesCol.slice(i + 1, treesRow.length);
+
+        const scenicScore = calcScenicScore(tree, left, right, up, down);
+
+        if (scenicScore > highestScenicScore) {
+          highestScenicScore = scenicScore;
+        }
+      }
+    }
+
+    console.log(highestScenicScore);
   } catch (err) {
     console.error(err);
   }
