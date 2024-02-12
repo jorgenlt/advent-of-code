c6173c4d465c020d2026d22df582ffa5e6c68d8f
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2024-02-12 20:02:32 +0800

    2016 day 7 part 1 + 2

--- 2016/day-07/partOne.js
+++ 2016/day-07/partOne.js
@@ -1,11 +1,59 @@
-  
 import { readFile } from "fs/promises";
+
+const hasAbba = (str) => {
+  for (let i = 3; i < str.length; i++) {
+    const char1 = str[i - 3];
+    const char2 = str[i - 2];
+    const char3 = str[i - 1];
+    const char4 = str[i];
+
+    if (char1 !== char2 && char1 === char4 && char2 === char3) {
+      return true;
+    }
+  }
+
+  return false;
+};
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const ips = (await readFile("input.txt", "utf-8")).trim().split("\n");
+
+    let ipCount = 0;
+
+    ips.forEach((ip) => {
+      const outsideBrackets = [];
+      const insideBrackets = [];
+
+      ip.split(/[\[\]]/g).forEach((seq, i) => {
+        if (i % 2 === 0) {
+          outsideBrackets.push(seq);
+        } else {
+          insideBrackets.push(seq);
+        }
+      });
+
+      let outsideBracketsHasAbba = false;
+      let insideBracketsHasAbba = false;
+
+      outsideBrackets.forEach((seq) => {
+        if (hasAbba(seq)) {
+          outsideBracketsHasAbba = true;
+        }
+      });
+
+      insideBrackets.forEach((seq) => {
+        if (hasAbba(seq)) {
+          insideBracketsHasAbba = true;
+        }
+      });
+
+      if (outsideBracketsHasAbba && !insideBracketsHasAbba) {
+        ipCount++;
+      }
+    });
+
+    console.log(ipCount);
   } catch (err) {
     console.error(err);
   }
--- 2016/day-07/partTwo.js
+++ 2016/day-07/partTwo.js
@@ -1,11 +1,88 @@
-  
 import { readFile } from "fs/promises";
+
+const getSequences = (ip) => {
+  const outsideBrackets = [];
+  const insideBrackets = [];
+
+  ip.split(/[\[\]]/g).forEach((seq, i) => {
+    if (i % 2 === 0) {
+      outsideBrackets.push(seq);
+    } else {
+      insideBrackets.push(seq);
+    }
+  });
+
+  return [outsideBrackets, insideBrackets];
+};
+
+const getAbas = (str) => {
+  const abas = [];
+
+  for (let i = 2; i < str.length; i++) {
+    const char1 = str[i - 2];
+    const char2 = str[i - 1];
+    const char3 = str[i];
+
+    if (char1 !== char2 && char1 === char3) {
+      abas.push(char1 + char2 + char3);
+    }
+  }
+
+  return abas;
+};
+
+const getAbasFromOutsideBrackets = (outsideBrackets) => {
+  const abas = [];
+
+  outsideBrackets.forEach((str) => {
+    const abasFromStr = getAbas(str);
+
+    if (abasFromStr.length > 0) {
+      abas.push(...abasFromStr);
+    }
+  });
+
+  return abas;
+};
+
+const hasBab = (insideBrackets, aba) => {
+  const [char1, char2] = aba.split("");
+  const bab = char2 + char1 + char2;
+
+  const foundBab = insideBrackets.some((str) => {
+    return str.includes(bab);
+  });
+
+  return foundBab;
+};
+
+const doesSupportSsl = (ip) => {
+  const [outsideBrackets, insideBrackets] = getSequences(ip);
+
+  let supportSsl = false;
+
+  const abas = getAbasFromOutsideBrackets(outsideBrackets);
+
+  if (abas.length > 0) {
+    abas.forEach((aba) => {
+      if (hasBab(insideBrackets, aba)) {
+        supportSsl = true;
+      }
+    });
+  }
+
+  return supportSsl;
+};
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const ips = (await readFile("input.txt", "utf-8")).trim().split("\n");
+
+    const ipCount = ips.reduce((count, ip) => {
+      return doesSupportSsl(ip) ? count + 1 : count;
+    }, 0);
+
+    console.log(ipCount);
   } catch (err) {
     console.error(err);
   }
