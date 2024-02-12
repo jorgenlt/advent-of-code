3f8d09084737ab732bd964c8246869f5297e2b99
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2024-02-12 17:09:09 +0800

    2016 day 6 part 1 + 2

--- 2016/day-06/partOne.js
+++ 2016/day-06/partOne.js
@@ -1,11 +1,49 @@
-  
 import { readFile } from "fs/promises";
+
+const findKeyWithHighestValue = (obj) => {
+  let maxKey = null;
+  let maxValue = -Infinity;
+
+  for (const key in obj) {
+    if (obj[key] > maxValue) {
+      maxKey = key;
+      maxValue = obj[key];
+    }
+  }
+
+  return maxKey;
+};
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const signals = (await readFile("input.txt", "utf-8")).trim().split("\n");
+
+    const charMap = new Map();
+
+    signals.forEach((signal) => {
+      for (let i = 0; i < signal.length; i++) {
+        const char = signal[i];
+        const pos = i.toString();
+
+        if (!charMap.has(pos)) {
+          charMap.set(pos, {});
+        }
+
+        if (!charMap.get(pos)[char]) {
+          charMap.get(pos)[char] = 1;
+        } else {
+          charMap.get(pos)[char]++;
+        }
+      }
+    });
+
+    let result = "";
+
+    [...charMap.entries()].forEach((pos) => {
+      result += findKeyWithHighestValue(pos[1]);
+    });
+
+    console.log(result);
   } catch (err) {
     console.error(err);
   }
--- 2016/day-06/partTwo.js
+++ 2016/day-06/partTwo.js
@@ -1,11 +1,49 @@
-  
 import { readFile } from "fs/promises";
+
+const findKeyWithLowestValue = (obj) => {
+  let minKey = null;
+  let minValue = Infinity;
+
+  for (const key in obj) {
+    if (obj[key] < minValue) {
+      minKey = key;
+      minValue = obj[key];
+    }
+  }
+
+  return minKey;
+};
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const signals = (await readFile("input.txt", "utf-8")).trim().split("\n");
+
+    const charMap = new Map();
+
+    signals.forEach((signal) => {
+      for (let i = 0; i < signal.length; i++) {
+        const char = signal[i];
+        const pos = i.toString();
+
+        if (!charMap.has(pos)) {
+          charMap.set(pos, {});
+        }
+
+        if (!charMap.get(pos)[char]) {
+          charMap.get(pos)[char] = 1;
+        } else {
+          charMap.get(pos)[char]++;
+        }
+      }
+    });
+
+    let result = "";
+
+    [...charMap.entries()].forEach((pos) => {
+      result += findKeyWithLowestValue(pos[1]);
+    });
+
+    console.log(result);
   } catch (err) {
     console.error(err);
   }
