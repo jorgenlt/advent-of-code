4b843d3d76ef9d4e0adaad2bc1136e4b07f798ad
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2024-02-12 11:59:48 +0800

    2016 day 5 part 1 + 2

--- 2016/day-05/partOne.js
+++ 2016/day-05/partOne.js
@@ -1,11 +1,36 @@
-  
 import { readFile } from "fs/promises";
+import crypto from "crypto";
+
+const findPassword = (str) => {
+  let password = "";
+  let i = 1;
+
+  for (let j = 0; j < 8; j++) {
+    let found = false;
+
+    while (!found) {
+      const hash = crypto
+        .createHash("md5")
+        .update(str + i.toString())
+        .digest("hex");
+
+      if (hash.startsWith("00000")) {
+        password += hash.charAt(5);
+        found = true;
+      }
+
+      i++;
+    }
+  }
+  return password;
+};
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = (await readFile("input.txt", "utf-8")).trim();
+
+    const password = findPassword(input);
+    console.log(password);
   } catch (err) {
     console.error(err);
   }
--- 2016/day-05/partTwo.js
+++ 2016/day-05/partTwo.js
@@ -1,11 +1,42 @@
-  
 import { readFile } from "fs/promises";
+import crypto from "crypto";
+
+const findPassword = (str) => {
+  const password = Array(8).fill(null);
+
+  let i = 1;
+
+  while (password.includes(null)) {
+    let found = false;
+
+    while (!found) {
+      const hash = crypto
+        .createHash("md5")
+        .update(str + i.toString())
+        .digest("hex");
+
+      if (hash.startsWith("00000")) {
+        const pos = Number(hash.charAt(5));
+        const char = hash.charAt(6);
+
+        if (pos < 8 && password[pos] === null) {
+          password[pos] = char;
+          found = true;
+        }
+      }
+
+      i++;
+    }
+  }
+  return password.join("");
+};
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = (await readFile("input.txt", "utf-8")).trim();
+
+    const password = findPassword(input);
+    console.log(password);
   } catch (err) {
     console.error(err);
   }
