87a66f85b0f918ad00aceb088e96f854d90ad1ae
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2024-02-13 20:14:20 +0800

    2016 day 9 part 2

--- 2016/day-09/partTwo.js
+++ 2016/day-09/partTwo.js
@@ -1,1 +1,52 @@
+import { readFile } from "fs/promises";
 
+function decompressedLength(str) {
+  let length = 0;
+  let i = 0;
+
+  while (i < str.length) {
+    if (str[i] === "(") {
+      // Found a marker
+      const markerEnd = str.indexOf(")", i);
+      const marker = str.slice(i + 1, markerEnd);
+      const [repeatLength, repeatCount] = marker.split("x").map(Number);
+
+      // Recursive call to get the length of the decompressed data within the marker
+      const nestedLength = decompressedLength(
+        str.slice(markerEnd + 1, markerEnd + 1 + repeatLength)
+      );
+
+      // Update the total length
+      length += repeatCount * nestedLength;
+
+      // Move the index past the marker and decompressed data
+      i = markerEnd + 1 + repeatLength;
+    } else {
+      // Regular character, increment length
+      length++;
+      i++;
+    }
+  }
+
+  return length;
+}
+
+const main = async () => {
+  try {
+    const compressed = (await readFile("input.txt", "utf-8"))
+      .trim()
+      .split("\n");
+
+    let length = 0;
+
+    compressed.forEach((line) => {
+      length += decompressedLength(line);
+    });
+
+    console.log(length);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+main();
