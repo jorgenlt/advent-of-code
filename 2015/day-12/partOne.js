00a2c1056fc5c35192447530576355ff83e01b83
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2024-05-11 22:42:29 +0200

    2015 day 12

--- 2015/day-12/partOne.js
+++ 2015/day-12/partOne.js
@@ -1,11 +1,20 @@
-  
 import { readFile } from "fs/promises";
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = (await readFile("input.txt", "utf-8")).trim();
+
+    const regex = /-?\d+/g;
+
+    const allNumbers = input.matchAll(regex);
+
+    let sum = 0;
+
+    for (const match of allNumbers) {
+      sum += Number(match[0]);
+    }
+
+    console.log(sum);
   } catch (err) {
     console.error(err);
   }
--- 2015/day-12/partTwo.js
+++ 2015/day-12/partTwo.js
@@ -1,11 +1,32 @@
-  
 import { readFile } from "fs/promises";
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = (await readFile("input.txt", "utf-8")).trim();
+
+    const regex = /-?\d+/g;
+
+    const parsedJson = JSON.parse(input, (key, value) => {
+      if (!Array.isArray(value)) {
+        if (Object.values(value).includes("red")) {
+          return {};
+        } else {
+          return value;
+        }
+      }
+
+      return value;
+    });
+
+    const allNumbers = JSON.stringify(parsedJson).matchAll(regex);
+
+    let sum = 0;
+
+    for (const match of allNumbers) {
+      sum += Number(match[0]);
+    }
+
+    console.log(sum);
   } catch (err) {
     console.error(err);
   }
