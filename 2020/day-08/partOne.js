5428e02f90f2833c243a9dccfe3ee5327f0920f8
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2024-05-11 05:23:39 +0200

    2020 day 08

--- 2020/day-08/partOne.js
+++ 2020/day-08/partOne.js
@@ -1,11 +1,39 @@
-  
 import { readFile } from "fs/promises";
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = (await readFile("input.txt", "utf-8")).trim().split("\n");
+
+    let charsStrLiteralsCount = 0;
+
+    let charsInMemoryCount = 0;
+
+    for (let i = 0; i < input.length; i++) {
+      const strLiteral = input[i];
+
+      charsStrLiteralsCount += strLiteral.length;
+
+      const str = strLiteral.slice(1, strLiteral.length - 1);
+
+      let charCount = 0;
+      let j = 0;
+      while (j < str.length) {
+        if (str[j] === "\\" && str[j + 1] === "x") {
+          charCount++;
+          j += 4;
+        } else if ((str[j] === "\\" && str[j + 1] === '"') || str[j] === "\\") {
+          charCount++;
+          j += 2;
+        } else {
+          charCount++;
+          j++;
+        }
+      }
+
+      charsInMemoryCount += charCount;
+    }
+
+    console.log(charsStrLiteralsCount - charsInMemoryCount);
   } catch (err) {
     console.error(err);
   }
--- 2020/day-08/partTwo.js
+++ 2020/day-08/partTwo.js
@@ -1,11 +1,24 @@
-  
 import { readFile } from "fs/promises";
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = (await readFile("input.txt", "utf-8")).trim().split("\n");
+
+    let charsStrLiteralsCount = 0;
+
+    let encodedcharsCount = 0;
+
+    for (let i = 0; i < input.length; i++) {
+      const strLiteral = input[i];
+
+      charsStrLiteralsCount += strLiteral.length;
+
+      // Using JSON.stringify to escape special characters and calculate
+      // the length of the encoded string
+      encodedcharsCount += JSON.stringify(strLiteral).length;
+    }
+
+    console.log(encodedcharsCount - charsStrLiteralsCount);
   } catch (err) {
     console.error(err);
   }
