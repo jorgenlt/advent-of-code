f55f0bd011c4a292394c6611349ae3d3832c851a
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2024-05-11 20:03:51 +0200

    2015 day 10

--- 2015/day-10/partOne.js
+++ 2015/day-10/partOne.js
@@ -1,11 +1,37 @@
-  
 import { readFile } from "fs/promises";
+
+const lookAndSay = (nums) => {
+  let result = "";
+
+  let count = 1;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === nums[i + 1]) {
+      count++;
+    } else {
+      result += count + nums[i];
+      count = 1;
+    }
+  }
+
+  return result;
+};
 
 const main = async () => {
   try {
     const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+
+    let currentResult = input;
+
+    for (let i = 1; i <= 50; i++) {
+      currentResult = lookAndSay(currentResult);
+
+      if (i === 40) {
+        console.log("40 times:", currentResult.length);
+      }
+    }
+
+    console.log("50 times:", currentResult.length);
   } catch (err) {
     console.error(err);
   }
