940f7d937abae2720fd9d0c165bfbe9ff4b335db
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2024-05-12 13:55:50 +0200

    2015 day 13

--- 2015/day-13/partTwo.js
+++ 2015/day-13/partTwo.js
@@ -1,11 +1,69 @@
-  
 import { readFile } from "fs/promises";
+import allPermutations from "../../utils/allPermutations.js";
+
+const parseInput = (input) => {
+  const parsed = {};
+
+  input.split("\n").forEach((line) => {
+    const guest = line.split(" ")[0];
+    const adjacentGuest = line.split(" ").at(-1).slice(0, -1);
+    const gainOrLose = line.match(/gain|lose/)[0];
+    let happiness =
+      (gainOrLose === "lose" ? -1 : 1) * Number(line.match(/\d+/)[0]);
+
+    if (!parsed[guest]) {
+      parsed[guest] = {};
+    }
+
+    parsed[guest][adjacentGuest] = happiness;
+
+    if (!parsed["me"]) {
+      parsed["me"] = {};
+    }
+
+    if (!parsed["me"][adjacentGuest]) {
+      parsed["me"][adjacentGuest] = 0;
+    }
+
+    if (!parsed[guest]["me"]) {
+      parsed[guest]["me"] = 0;
+    }
+  });
+
+  return parsed;
+};
+
+const getTotalHappiness = (guests, tableData) => {
+  let sum = 0;
+
+  for (let i = 0; i < guests.length; i++) {
+    const guest = guests[i];
+    const left = i === 0 ? guests.at(-1) : guests[i - 1];
+    const right = i === guests.length - 1 ? guests[0] : guests[i + 1];
+    sum += tableData[guest][left] + tableData[guest][right];
+  }
+  return sum;
+};
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = (await readFile("input.txt", "utf-8")).trim();
+
+    const tableData = parseInput(input);
+
+    const permutations = allPermutations(Object.keys(tableData));
+
+    let highestHappiness = 0;
+
+    permutations.forEach((guests) => {
+      const totalHappiness = getTotalHappiness(guests, tableData);
+
+      if (totalHappiness > highestHappiness) {
+        highestHappiness = totalHappiness;
+      }
+    });
+
+    console.log("highestHappiness:", highestHappiness);
   } catch (err) {
     console.error(err);
   }
