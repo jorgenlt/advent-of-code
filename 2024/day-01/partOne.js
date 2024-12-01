5d69898a123b5d0435493013008c4830dc3dc2ad
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2024-12-01 15:01:47 +0800

    2024 day 1

--- 2024/day-01/partOne.js
+++ 2024/day-01/partOne.js
@@ -1,11 +1,34 @@
-  
 import { readFile } from "fs/promises";
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = (await readFile("input.txt", "utf-8")).trim().split("\n");
+
+    // Create two arrays: left list and right list.
+    const leftList = [];
+    const rightList = [];
+
+    input.forEach((line) => {
+      const leftNum = Number(line.match(/\d+/g)[0]);
+      const rightNum = Number(line.match(/\d+/g)[1]);
+      leftList.push(leftNum);
+      rightList.push(rightNum);
+    });
+
+    // Sort the lists from smallest to largest Then sort from smallest to largest
+    leftList.sort((a, b) => a - b);
+    rightList.sort((a, b) => a - b);
+
+    let totalDistance = 0;
+
+    for (let i = 0; i < leftList.length; i++) {
+      const leftNum = leftList[i];
+      const rightNum = rightList[i];
+
+      totalDistance += Math.abs(leftNum - rightNum);
+    }
+
+    console.log(totalDistance);
   } catch (err) {
     console.error(err);
   }
