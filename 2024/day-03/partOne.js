24023bd3d604ac953ffe1cc3a711a901331b71de
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2024-12-03 14:05:45 +0800

    2024 day 3

--- 2024/day-03/partOne.js
+++ 2024/day-03/partOne.js
@@ -1,11 +1,19 @@
-  
 import { readFile } from "fs/promises";
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = (await readFile("input.txt", "utf-8")).trim();
+
+    const regex = /mul\(\d+,\d+\)/gm;
+    const instructions = input.match(regex);
+
+    const result = instructions.reduce((count, instruction) => {
+      const [num1, num2] = instruction.match(/\d+/g).map(Number);
+
+      return count + num1 * num2;
+    }, 0);
+
+    console.log(result);
   } catch (err) {
     console.error(err);
   }
--- 2024/day-03/partTwo.js
+++ 2024/day-03/partTwo.js
@@ -1,11 +1,27 @@
-  
 import { readFile } from "fs/promises";
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = (await readFile("input.txt", "utf-8")).trim();
+
+    const regex = /mul\(\d+,\d+\)|don't\(\)|do\(\)/gm;
+    const instructions = input.match(regex);
+
+    let sum = 0;
+    let validMul = true;
+
+    for (const instruction of instructions) {
+      if (instruction === "do()") {
+        validMul = true;
+      } else if (instruction === "don't()") {
+        validMul = false;
+      } else if (validMul) {
+        const [num1, num2] = instruction.match(/\d+/g).map(Number);
+        sum += num1 * num2;
+      }
+    }
+
+    console.log(sum);
   } catch (err) {
     console.error(err);
   }
