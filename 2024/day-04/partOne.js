06e809bcab3e2d34d791d2a4a5c5d125338ac402
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2024-12-04 16:13:10 +0800

    2024 day 4

--- 2024/day-04/partOne.js
+++ 2024/day-04/partOne.js
@@ -1,11 +1,59 @@
-  
 import { readFile } from "fs/promises";
+import reverseString from "../../utils/reverseString.js";
+
+const checkForXmas = (word) =>
+  word === "XMAS" || reverseString(word) === "XMAS";
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const grid = (await readFile("input.txt", "utf-8"))
+      .trim()
+      .split("\n")
+      .map((line) => line.split(""));
+
+    let wordCount = 0;
+    const maxRow = grid.length;
+    const maxCol = grid[0].length;
+
+    for (let i = 0; i < maxRow; i++) {
+      for (let j = 0; j < maxCol; j++) {
+        // Horizontally
+        if (j <= maxCol - 4) {
+          const horizontal =
+            grid[i][j] + grid[i][j + 1] + grid[i][j + 2] + grid[i][j + 3];
+          if (checkForXmas(horizontal)) wordCount++;
+        }
+
+        // Vertically
+        if (i <= maxRow - 4) {
+          const vertical =
+            grid[i][j] + grid[i + 1][j] + grid[i + 2][j] + grid[i + 3][j];
+          if (checkForXmas(vertical)) wordCount++;
+        }
+
+        // Diagonally right
+        if (i <= maxRow - 4 && j <= maxCol - 4) {
+          const diagonalRight =
+            grid[i][j] +
+            grid[i + 1][j + 1] +
+            grid[i + 2][j + 2] +
+            grid[i + 3][j + 3];
+          if (checkForXmas(diagonalRight)) wordCount++;
+        }
+
+        // Diagonally left
+        if (i <= maxRow - 4 && j >= 3) {
+          const diagonalLeft =
+            grid[i][j] +
+            grid[i + 1][j - 1] +
+            grid[i + 2][j - 2] +
+            grid[i + 3][j - 3];
+          if (checkForXmas(diagonalLeft)) wordCount++;
+        }
+      }
+    }
+
+    console.log(wordCount);
   } catch (err) {
     console.error(err);
   }
--- 2024/day-04/partTwo.js
+++ 2024/day-04/partTwo.js
@@ -1,11 +1,33 @@
-  
 import { readFile } from "fs/promises";
+import reverseString from "../../utils/reverseString.js";
+
+const checkForMas = (word) => word === "MAS" || reverseString(word) === "MAS";
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const grid = (await readFile("input.txt", "utf-8"))
+      .trim()
+      .split("\n")
+      .map((line) => line.split(""));
+
+    let xMasCount = 0;
+    const maxRow = grid.length;
+    const maxCol = grid[0].length;
+
+    for (let i = 0; i < maxRow - 2; i++) {
+      for (let j = 0; j < maxCol - 2; j++) {
+        const diagonalRight =
+          grid[i][j] + grid[i + 1][j + 1] + grid[i + 2][j + 2];
+
+        const diagonalLeft =
+          grid[i][j + 2] + grid[i + 1][j + 1] + grid[i + 2][j];
+
+        if (checkForMas(diagonalRight) && checkForMas(diagonalLeft))
+          xMasCount++;
+      }
+    }
+
+    console.log(xMasCount);
   } catch (err) {
     console.error(err);
   }
