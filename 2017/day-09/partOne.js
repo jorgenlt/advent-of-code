a6daac3a9114decab6fec1d3786b03231956de38
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2025-02-06 14:15:06 +0800

    2017 day 09 part 1

--- 2017/day-09/partOne.js
+++ 2017/day-09/partOne.js
@@ -1,11 +1,50 @@
-  
 import { readFile } from "fs/promises";
+
+const solvePuzzle = (input) => {
+  let totalScore = 0;
+  let depth = 0;
+  let inGarbage = false;
+  let ignoreNextChar = false;
+
+  for (let i = 0; i < input.length; i++) {
+    const char = input[i];
+
+    if (ignoreNextChar) {
+      ignoreNextChar = false;
+      continue;
+    }
+
+    if (char === "!") {
+      ignoreNextChar = true;
+    } else if (inGarbage) {
+      if (char === ">") {
+        inGarbage = false;
+      }
+    } else {
+      switch (char) {
+        case "<":
+          inGarbage = true;
+          break;
+        case "{":
+          depth++;
+          break;
+        case "}":
+          totalScore += depth;
+          depth--;
+          break;
+      }
+    }
+  }
+
+  console.log(totalScore);
+  return totalScore;
+};
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = (await readFile("input.txt", "utf-8")).trim();
+
+    solvePuzzle(input);
   } catch (err) {
     console.error(err);
   }
