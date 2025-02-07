c5c18d84bb2d6740ca9488961e49fea2d9d46cda
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2025-02-07 15:53:21 +0800

    2017 day 12 part 2

--- 2017/day-12/partTwo.js
+++ 2017/day-12/partTwo.js
@@ -1,11 +1,53 @@
-  
 import { readFile } from "fs/promises";
+
+const createGraph = (input) => {
+  const graph = new Map();
+
+  input.split("\n").forEach((l) => {
+    const numbers = l.match(/\d+/g).map(Number);
+    const programId = numbers.shift();
+
+    graph.set(programId, numbers);
+  });
+
+  return graph;
+};
+
+const solvePuzzle = (input) => {
+  const globalVisited = new Set();
+  const graph = createGraph(input);
+  let groupCount = 0;
+
+  for (const key of graph.keys()) {
+    if (!globalVisited.has(key)) {
+      groupCount++;
+    }
+    // BFS
+    const queue = [key];
+
+    while (queue.length > 0) {
+      const vertex = queue.shift();
+
+      if (!globalVisited.has(vertex)) {
+        globalVisited.add(vertex);
+
+        for (const neighbor of graph.get(vertex)) {
+          if (!globalVisited.has(neighbor)) {
+            queue.push(neighbor);
+          }
+        }
+      }
+    }
+  }
+
+  return groupCount;
+};
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = (await readFile("input.txt", "utf-8")).trim();
+
+    console.log(solvePuzzle(input));
   } catch (err) {
     console.error(err);
   }
