e66060377b073e63027dde039a14d45897520c8b
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2025-02-08 18:39:46 +0800

    2017 day 17 part 2

--- 2017/day-17/partTwo.js
+++ 2017/day-17/partTwo.js
@@ -1,11 +1,23 @@
-  
-import { readFile } from "fs/promises";
+const solvePuzzle = (steps) => {
+  let currentPos = 0;
+  let valueAfterZero = 0;
+
+  for (let i = 1; i <= 50000000; i++) {
+    currentPos = ((currentPos + steps) % i) + 1;
+
+    if (currentPos === 1) {
+      valueAfterZero = i;
+    }
+  }
+
+  return valueAfterZero;
+};
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const steps = 371;
+
+    console.log(solvePuzzle(steps));
   } catch (err) {
     console.error(err);
   }
