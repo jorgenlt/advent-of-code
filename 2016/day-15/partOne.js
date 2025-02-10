fb9968942b2885778c011626a1233e7c6c7cc58b
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2025-02-10 20:22:34 +0800

    2016 day 15 part 1

--- 2016/day-15/partOne.js
+++ 2016/day-15/partOne.js
@@ -1,11 +1,51 @@
-  
 import { readFile } from "fs/promises";
+
+const parseInput = (input) => {
+  const disks = [];
+
+  input.split("\n").forEach((l) => {
+    const [diskNum, posNum, _, initialPos] = l.match(/\d+/g).map(Number);
+    disks.push({
+      diskNum,
+      posNum,
+      initialPos,
+    });
+  });
+
+  return disks;
+};
+
+// For the slot to be in place, we need:
+// (initial position + T + i) mod (number of positions) = 0.
+const isSlotInPlace = (initialPos, time, diskNum, posNum) => {
+  if ((initialPos + time + diskNum) % posNum === 0) {
+    return true;
+  }
+  return false;
+};
+
+const solvePuzzle = (input) => {
+  const disks = parseInput(input);
+
+  let allDisksAligned = false;
+  let time = 0;
+
+  while (!allDisksAligned) {
+    allDisksAligned = disks.every((disk) =>
+      isSlotInPlace(disk.initialPos, time, disk.diskNum, disk.posNum)
+    );
+
+    time++;
+  }
+
+  return time - 1;
+};
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = (await readFile("input.txt", "utf-8")).trim();
+
+    console.log(solvePuzzle(input));
   } catch (err) {
     console.error(err);
   }
