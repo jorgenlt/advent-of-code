a054160977e9d1d9200d4a216e26a37aa71ea157
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2025-02-11 14:49:34 +0800

    2016 day 17 part 2

--- 2016/day-17/partTwo.js
+++ 2016/day-17/partTwo.js
@@ -1,11 +1,98 @@
-  
-import { readFile } from "fs/promises";
+import crypto from "crypto";
+
+const md5 = (salt) => crypto.createHash("md5").update(salt).digest("hex");
+
+const directions = [
+  { dx: 0, dy: -1, directionChar: "U" }, // Up
+  { dx: 0, dy: 1, directionChar: "D" }, // Down
+  { dx: -1, dy: 0, directionChar: "L" }, // Left
+  { dx: 1, dy: 0, directionChar: "R" }, // Right
+];
+
+const findLongestPath = (start, end, passcode) => {
+  const queue = [];
+  queue.push({ ...start, path: "" });
+
+  let longestPath = "";
+
+  while (queue.length > 0) {
+    const currentLocation = queue.shift();
+
+    if (currentLocation.x === end.x && currentLocation.y === end.y) {
+      if (currentLocation.path.length > longestPath.length) {
+        longestPath = currentLocation.path;
+      }
+      continue;
+    }
+
+    for (const direction of directions) {
+      const newX = direction.dx + currentLocation.x;
+      const newY = direction.dy + currentLocation.y;
+
+      if (newX < 0 || newX > 3 || newY < 0 || newY > 3) {
+        continue;
+      }
+
+      const hash = md5(passcode + currentLocation.path);
+
+      const openChars = "bcdef";
+
+      switch (direction.directionChar) {
+        case "U":
+          if (openChars.includes(hash[0])) {
+            queue.push({
+              x: newX,
+              y: newY,
+              path: currentLocation.path + direction.directionChar,
+            });
+          }
+          break;
+        case "D":
+          if (openChars.includes(hash[1])) {
+            queue.push({
+              x: newX,
+              y: newY,
+              path: currentLocation.path + direction.directionChar,
+            });
+          }
+          break;
+        case "L":
+          if (openChars.includes(hash[2])) {
+            queue.push({
+              x: newX,
+              y: newY,
+              path: currentLocation.path + direction.directionChar,
+            });
+          }
+          break;
+        case "R":
+          if (openChars.includes(hash[3])) {
+            queue.push({
+              x: newX,
+              y: newY,
+              path: currentLocation.path + direction.directionChar,
+            });
+          }
+          break;
+
+        default:
+          break;
+      }
+    }
+  }
+
+  return longestPath.length;
+};
+
+const solvePuzzle = (passcode) => {
+  return findLongestPath({ x: 0, y: 0 }, { x: 3, y: 3 }, passcode);
+};
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = "qljzarfv";
+
+    console.log(solvePuzzle(input));
   } catch (err) {
     console.error(err);
   }
