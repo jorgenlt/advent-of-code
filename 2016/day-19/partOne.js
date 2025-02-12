dd89797f112164693f6116fbb4408779be0833a5
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2025-02-12 08:44:09 +0800

    2016 day 19 part 1

--- 2016/day-19/partOne.js
+++ 2016/day-19/partOne.js
@@ -1,11 +1,26 @@
-  
-import { readFile } from "fs/promises";
+const findLargestPowerOfTwo = (participants) => {
+  let p = parseInt(Math.log(participants) / Math.log(2), 10);
+  return Math.pow(2, p);
+};
+
+const solveJosephusProblem = (participants) => {
+  // Finding the largest power of 2 that is less or equal to
+  // number of participants
+  const largestPowerOfTwo = findLargestPowerOfTwo(participants);
+
+  // Solving Josephus problem
+  return 2 * (participants - largestPowerOfTwo) + 1;
+};
+
+const solvePuzzle = (input) => {
+  return solveJosephusProblem(input);
+};
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = 3018458;
+
+    console.log(solvePuzzle(input));
   } catch (err) {
     console.error(err);
   }
