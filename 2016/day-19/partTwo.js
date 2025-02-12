750759a1eb28bc8e8ae430ea6692243cc814702e
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2025-02-12 11:42:58 +0800

    2016 day 19 part 2

--- 2016/day-19/partTwo.js
+++ 2016/day-19/partTwo.js
@@ -1,11 +1,23 @@
-  
-import { readFile } from "fs/promises";
+import findLargestPower from "../../utils/findLargestPower.js";
+
+const solveJosephusProblem = (participants) => {
+  // Finding the largest power of 2 that is less or equal to
+  // number of participants
+  const largestPowerOfThree = findLargestPower(participants, 3);
+
+  // Solving Josephus problem
+  return participants - largestPowerOfThree;
+};
+
+const solvePuzzle = (input) => {
+  return solveJosephusProblem(input);
+};
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = 3018458;
+
+    console.log(solvePuzzle(input));
   } catch (err) {
     console.error(err);
   }
