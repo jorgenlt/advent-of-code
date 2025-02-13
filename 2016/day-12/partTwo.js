a944f4622d2258c093343a0ecec079aed1e59f52
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2025-02-13 20:05:20 +0800

    2016 day 12 part 2

--- 2016/day-12/partTwo.js
+++ 2016/day-12/partTwo.js
@@ -1,11 +1,60 @@
-  
 import { readFile } from "fs/promises";
+
+const parseInput = (input) => {
+  return input.split("\n");
+};
+
+const solvePuzzle = (input) => {
+  const instructions = parseInput(input);
+
+  const register = new Map([
+    ["a", 0],
+    ["b", 0],
+    ["c", 1], // Change c to 1 for Part Two.
+    ["d", 0],
+  ]);
+
+  let i = 0;
+
+  while (i < instructions.length) {
+    const [instruction, x, y] = instructions[i].split(" ");
+
+    switch (instruction) {
+      case "cpy":
+        const cpyValue = !isNaN(x) ? Number(x) : register.get(x);
+        register.set(y, cpyValue);
+        i++;
+        break;
+      case "inc":
+        register.set(x, register.get(x) + 1);
+        i++;
+        break;
+      case "dec":
+        register.set(x, register.get(x) - 1);
+        i++;
+        break;
+      case "jnz":
+        const jnzValue = !isNaN(x) ? Number(x) : register.get(x);
+        if (jnzValue !== 0) {
+          i += Number(y);
+        } else {
+          i++;
+        }
+        break;
+      default:
+        i++;
+        break;
+    }
+  }
+
+  return register.get("a");
+};
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = (await readFile("input.txt", "utf-8")).trim();
+
+    console.log(solvePuzzle(input));
   } catch (err) {
     console.error(err);
   }
