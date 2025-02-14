e12c4c2cb2f2b93d5856d7679e18413d5fdad801
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2025-02-14 17:15:05 +0800

    2018 day 8 part 1

--- 2018/day-08/partOne.js
+++ 2018/day-08/partOne.js
@@ -1,11 +1,39 @@
-  
 import { readFile } from "fs/promises";
+
+const parseInput = (input) => input.split(" ").map(Number);
+
+const parseNode = (data, startIndex) => {
+  let index = startIndex;
+  const numChildren = data[index++];
+  const numMetadata = data[index++];
+  let metadataSum = 0;
+
+  for (let i = 0; i < numChildren; i++) {
+    const result = parseNode(data, index);
+    metadataSum += result.metadataSum;
+    index = result.index;
+  }
+
+  for (let i = 0; i < numMetadata; i++) {
+    metadataSum += data[index++];
+  }
+
+  return { metadataSum, index };
+};
+
+const solvePuzzle = (input) => {
+  const data = parseInput(input);
+
+  const result = parseNode(data, 0);
+
+  return result.metadataSum;
+};
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = (await readFile("input.txt", "utf-8")).trim();
+
+    console.log(solvePuzzle(input));
   } catch (err) {
     console.error(err);
   }
