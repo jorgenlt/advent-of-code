8c3a0829843473d615e2bd0adac7641849362043
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2025-02-18 15:43:09 +0800

    2019 day 6 part 1

--- 2019/day-06/partOne.js
+++ 2019/day-06/partOne.js
@@ -1,11 +1,39 @@
-  
 import { readFile } from "fs/promises";
+
+const parseInput = (input) => {
+  const orbits = new Map();
+
+  input.split("\n").forEach((l) => {
+    const [a, b] = l.split(")");
+
+    orbits.set(b, a);
+  });
+
+  return orbits;
+};
+
+const solvePuzzle = (input) => {
+  const orbits = parseInput(input);
+
+  let totalOrbits = 0;
+
+  for (const [child, parent] of orbits) {
+    let current = parent;
+
+    while (current) {
+      totalOrbits++;
+      current = orbits.get(current);
+    }
+  }
+
+  return totalOrbits;
+};
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = (await readFile("input.txt", "utf-8")).trim();
+
+    console.log(solvePuzzle(input));
   } catch (err) {
     console.error(err);
   }
