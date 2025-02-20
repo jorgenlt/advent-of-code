9421cefe4d20d3cb314148cb1bb20a4c33e94d2c
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2025-02-21 07:14:20 +0800

    2020 day 10 part 1

--- 2020/day-10/partOne.js
+++ 2020/day-10/partOne.js
@@ -1,11 +1,41 @@
-  
 import { readFile } from "fs/promises";
+
+const parseInput = (input) => {
+  return input
+    .split("\n")
+    .map(Number)
+    .sort((a, b) => a - b);
+};
+
+const solvePuzzle = (input) => {
+  const adapters = parseInput(input);
+
+  let oneCount = 0;
+  let threeCount = 0;
+
+  for (let i = 0; i < adapters.length; i++) {
+    const prev = i > 0 ? adapters[i - 1] : 0;
+    const curr = adapters[i];
+
+    if (curr - prev === 1) {
+      oneCount++;
+    } else if (curr - prev === 3) {
+      threeCount++;
+    }
+
+    if (i === adapters.length - 1) {
+      threeCount++;
+    }
+  }
+
+  return oneCount * threeCount;
+};
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = (await readFile("input.txt", "utf-8")).trim();
+
+    console.log(solvePuzzle(input));
   } catch (err) {
     console.error(err);
   }
