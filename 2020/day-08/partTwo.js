cd6d216df1305a8a66b2c30ad72e5390189f114a
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2025-02-20 09:44:18 +0800

    rm 2020 day 08

--- 2020/day-08/partTwo.js
+++ 2020/day-08/partTwo.js
@@ -1,24 +1,11 @@
+  
 import { readFile } from "fs/promises";
 
 const main = async () => {
   try {
-    const input = (await readFile("input.txt", "utf-8")).trim().split("\n");
-
-    let charsStrLiteralsCount = 0;
-
-    let encodedcharsCount = 0;
-
-    for (let i = 0; i < input.length; i++) {
-      const strLiteral = input[i];
-
-      charsStrLiteralsCount += strLiteral.length;
-
-      // Using JSON.stringify to escape special characters and calculate
-      // the length of the encoded string
-      encodedcharsCount += JSON.stringify(strLiteral).length;
-    }
-
-    console.log(encodedcharsCount - charsStrLiteralsCount);
+    const input = await readFile("input.txt", "utf-8");
+    
+    console.log(input);
   } catch (err) {
     console.error(err);
   }
