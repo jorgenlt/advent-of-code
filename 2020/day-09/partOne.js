77091e98c191107da570c8ec1d7acbca6fb26336
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2025-02-20 15:20:56 +0800

    2020 day 9 part 1

--- 2020/day-09/partOne.js
+++ 2020/day-09/partOne.js
@@ -1,11 +1,46 @@
-  
 import { readFile } from "fs/promises";
+
+const parseInput = (input) => {
+  return input.split("\n").map(Number);
+};
+
+const isValid = (prevNums, number) => {
+  for (let i = 0; i < prevNums.length; i++) {
+    const numA = prevNums[i];
+
+    for (let j = 0; j < prevNums.length; j++) {
+      const numB = prevNums[j];
+
+      if (numA === numB) continue;
+
+      if (numA + numB === number) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+};
+
+const solvePuzzle = (input) => {
+  const numbers = parseInput(input);
+  const preambleSize = 25;
+
+  for (let i = preambleSize; i < numbers.length; i++) {
+    const number = numbers[i];
+    const prevNums = numbers.slice(i - preambleSize, i);
+
+    if (!isValid(prevNums, number)) {
+      return number;
+    }
+  }
+};
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = (await readFile("input.txt", "utf-8")).trim();
+
+    console.log(solvePuzzle(input));
   } catch (err) {
     console.error(err);
   }
