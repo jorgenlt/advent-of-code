69fd3c332585b644b0604bcb3d85aab90395e4a8
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2025-02-22 08:23:54 +0800

    2020 day 13 part 1

--- 2020/day-13/partOne.js
+++ 2020/day-13/partOne.js
@@ -1,11 +1,41 @@
-  
 import { readFile } from "fs/promises";
+
+const parseInput = (input) => {
+  const [section1, section2] = input.split("\n");
+
+  return [Number(section1), section2.match(/\d+/g).map(Number)];
+};
+
+const getHighestDecimalNumber = (numbers) => {
+  return numbers.reduce((max, current) => {
+    const maxDecimal = max - Math.floor(max);
+    const currentDecimal = current - Math.floor(current);
+
+    return currentDecimal > maxDecimal ? current : max;
+  });
+};
+
+const solvePuzzle = (input) => {
+  const [departTime, busIds] = parseInput(input);
+
+  const totalDeparts = busIds.map((id) => departTime / id);
+
+  const highestDecimalNumber = getHighestDecimalNumber(totalDeparts);
+  const indexHigestDecimal = totalDeparts.indexOf(highestDecimalNumber);
+  const busId = busIds[indexHigestDecimal];
+  const departTimeBus = busId * Math.ceil(highestDecimalNumber);
+  const waitTime = departTimeBus - departTime;
+
+  const result = waitTime * busId;
+
+  return result;
+};
 
 const main = async () => {
   try {
     const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+
+    console.log(solvePuzzle(input));
   } catch (err) {
     console.error(err);
   }
