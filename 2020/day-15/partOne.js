e377d6f4fb3b494591d967f84666fb37833d902f
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2025-02-22 16:28:32 +0800

    2020 day 15 part 1

--- 2020/day-15/partOne.js
+++ 2020/day-15/partOne.js
@@ -1,11 +1,47 @@
-  
-import { readFile } from "fs/promises";
+const parseInput = (input) => {
+  return input.split(",").map(Number);
+};
+
+const solvePuzzle = (input) => {
+  const startingNumbers = parseInput(input);
+
+  const register = new Map();
+
+  for (let i = 0; i < startingNumbers.length; i++) {
+    const number = startingNumbers[i];
+    register.set(number, [i + 1]);
+  }
+
+  let prevNum = startingNumbers[startingNumbers.length - 1];
+
+  for (let turn = startingNumbers.length + 1; turn <= 2020; turn++) {
+    const prevSpoken = register.get(prevNum);
+    let nextNum;
+
+    if (prevSpoken.length === 1) {
+      nextNum = 0;
+    } else {
+      const [secondLast, last] = prevSpoken.slice(-2);
+      nextNum = last - secondLast;
+    }
+
+    if (register.has(nextNum)) {
+      register.set(nextNum, [...register.get(nextNum), turn]);
+    } else {
+      register.set(nextNum, [turn]);
+    }
+
+    prevNum = nextNum;
+  }
+
+  return prevNum;
+};
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = "11,18,0,20,1,7,16";
+
+    console.log(solvePuzzle(input));
   } catch (err) {
     console.error(err);
   }
