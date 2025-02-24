407ddba343f0070fe1c9a27cf93761d69b1fb5df
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2025-02-24 08:19:50 +0800

    2020 day 18 part 1

--- 2020/day-18/partOne.js
+++ 2020/day-18/partOne.js
@@ -1,11 +1,62 @@
-  
 import { readFile } from "fs/promises";
+
+const parseInput = (input) => {
+  return input
+    .replace(/ /g, "")
+    .split("\n")
+    .map((e) => e.split(""));
+};
+
+const calculate = (tokens) => {
+  let currentValue = 0;
+  let operator = "+";
+
+  while (tokens.length > 0) {
+    const token = tokens.shift();
+
+    if (!isNaN(Number(token))) {
+      const num = Number(token);
+
+      if (operator === "+") {
+        currentValue += num;
+      } else if (operator === "*") {
+        currentValue *= num;
+      }
+    } else if (token === "+" || token === "*") {
+      operator = token;
+    } else if (token === "(") {
+      const num = calculate(tokens);
+
+      if (operator === "+") {
+        currentValue += num;
+      } else if (operator === "*") {
+        currentValue *= num;
+      }
+    } else if (token === ")") {
+      return currentValue;
+    }
+  }
+
+  return currentValue;
+};
+
+const solvePuzzle = (input) => {
+  const expressions = parseInput(input);
+
+  let sum = 0;
+
+  for (const tokens of expressions) {
+    sum += calculate(tokens);
+  }
+
+  return sum;
+};
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = (await readFile("input.txt", "utf-8")).trim();
+
+    console.log(solvePuzzle(input));
   } catch (err) {
     console.error(err);
   }
