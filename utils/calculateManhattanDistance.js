5801b1daf2a799326827ac38d9cf3e2909958f4c
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2025-03-22 09:27:10 +0700

    update utils

--- utils/calculateManhattanDistance.js
+++ utils/calculateManhattanDistance.js
@@ -1,7 +1,7 @@
 // from start [x1, y1] to end [x2, y2]
 const calculateManhattanDistance = (a, b) => {
-  var distance = 0;
-  var dimensions = Math.max(a.length, b.length);
+  let distance = 0;
+  const dimensions = Math.max(a.length, b.length);
   for (var i = 0; i < dimensions; i++) {
     distance += Math.abs((b[i] || 0) - (a[i] || 0));
   }
