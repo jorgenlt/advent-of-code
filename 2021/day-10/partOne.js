8a448507be2488d427f584d78c86cdb18e02de5b
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2025-03-25 23:09:44 +0700

    2021 day 10 part 1

--- 2021/day-10/partOne.js
+++ 2021/day-10/partOne.js
@@ -1,11 +1,42 @@
-  
 import { readFile } from "fs/promises";
+
+const parseInput = (input) => {
+  return input.split("\n").map((l) => l.split(""));
+};
+
+const scoring = {
+  curlyBracket: 1197,
+  squareBracket: 57,
+  parenthesis: 3,
+  angleBracket: 25137,
+};
+
+const evaluateLine = (line) => {
+  
+}
+
+console.log(evaluateLine("{([(<{}[<>[]}>{[]{[(<()>".split("")))
+
+const solvePuzzle = (input) => {
+  const lines = parseInput(input);
+
+
+  let sum = 0;
+
+  for (const line of lines) {
+    const score = evaluateLine(line);
+
+    sum += score;
+  }
+
+  return sum;
+};
 
 const main = async () => {
   try {
-    const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+    const input = (await readFile("test.txt", "utf-8")).trim();
+
+    // console.log(solvePuzzle(input));
   } catch (err) {
     console.error(err);
   }
