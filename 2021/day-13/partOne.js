e27dd9fa984d2f7bae8f695c171d7777ab9c3ad6
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2025-04-02 18:43:47 +0700

    2021 day 13 part 1

--- 2021/day-13/partOne.js
+++ 2021/day-13/partOne.js
@@ -1,4 +1,5 @@
 import { readFile } from "fs/promises";
+import createGrid from "../../utils/createGrid.js";
 
 const parseInput = (input) => {
   const [section1, section2] = input.trim().split("\n\n");
@@ -15,18 +16,71 @@ const parseInput = (input) => {
   return [dots, instructions];
 };
 
-const createGrid = (dots) => {};
+const populateGrid = (dots) => {
+  let maxX = 0;
+  let maxY = 0;
 
-const foldGrid = (grid, instruction) => {};
+  for (const [x, y] of dots) {
+    if (x > maxX) maxX = x;
+    if (y > maxY) maxY = y;
+  }
 
-const calcDots = (grid) => {};
+  const grid = createGrid(maxX + 1, maxY + 1, ".");
+
+  for (const [x, y] of dots) {
+    grid[y][x] = "#";
+  }
+
+  return grid;
+};
+
+const foldGrid = (grid, instruction) => {
+  console.log(grid.length)
+
+  const [direction, foldLine] = instruction
+  
+  if (direction === "y") {
+    const top = grid.slice(0, foldLine);
+    const bottom = grid.slice(foldLine + 1).reverse();
+  
+    for (let y = 0; y < top.length; y++) {
+      for (let x = 0; x < top[0].length; x++) {
+        if (bottom[y][x] === "#") top[y][x] = "#"        
+      }
+    }
+
+    return top;
+  }
+
+  if (direction === "x") {
+    
+  }
+};
+
+const calcDots = (grid) => {
+  let count = 0;
+
+  for (const row of grid) {
+    for (const cell of row) {
+      if (cell === "#") count++;
+    }
+  }
+
+  return count;
+};
 
 const solvePuzzle = (input) => {
   const [dots, instructions] = parseInput(input);
 
-  const grid = createGrid(dots);
+  const grid = populateGrid(dots);
 
-  return calcDots(foldGrid(grid, instructions[0]));
+  const fold1 = foldGrid(grid, instructions[0]);
+  console.log(calcDots(fold1))
+  // console.log(fold1);
+
+  // const fold2 = foldGrid(fold1, instructions[1]);
+
+  // return calcDots(foldGrid(grid, instructions[0]));
 };
 
 const main = async () => {
