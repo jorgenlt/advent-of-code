8c0d0fd2e8cead8fc4d9584a3a788fab97301a59
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2025-04-16 23:24:04 +0200

    2021 day 16 part 1

--- 2021/day-16/partOne.js
+++ 2021/day-16/partOne.js
@@ -1,11 +1,14 @@
-  
 import { readFile } from "fs/promises";
+
+const parseInput = (input) => {};
+
+const solvePuzzle = (input) => {};
 
 const main = async () => {
   try {
     const input = await readFile("input.txt", "utf-8");
-    
-    console.log(input);
+
+    console.log(solvePuzzle(input));
   } catch (err) {
     console.error(err);
   }
