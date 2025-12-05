67a5d9a526ca4d1065cf6cb09f2534accc9d37d5
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2025-12-05 14:58:10 +0800

    2025 day 1 part 1

--- 2025/day-01/partOne.js
+++ 2025/day-01/partOne.js
@@ -1,14 +1,57 @@
-  
 import { readFile } from "fs/promises";
 
-const parseInput = (input) => {};
+const parseInput = (input) => {
+  return input.split("\n");
+};
 
-const solvePuzzle = (input) => {};
+const turnDial = (current, direction, steps) => {
+  let newPosition = current;
+
+  for (let i = 0; i < steps; i++) {
+    if (direction === "L") {
+      if (newPosition === 0) {
+        newPosition = 99;
+      } else {
+        newPosition--;
+      }
+    } else {
+      if (newPosition === 99) {
+        newPosition = 0;
+      } else {
+        newPosition++;
+      }
+    }
+  }
+
+  return newPosition;
+};
+
+const solvePuzzle = (input) => {
+  const instructions = parseInput(input);
+
+  let current = 50;
+  let zeroCount = 0;
+
+  for (let instruction of instructions) {
+    const direction = instruction[0];
+    const steps = Number(instruction.slice(1));
+
+    let newPosition = turnDial(current, direction, steps);
+
+    if (newPosition === 0) {
+      zeroCount++;
+    }
+
+    current = newPosition;
+  }
+
+  return zeroCount;
+};
 
 const main = async () => {
   try {
     const input = (await readFile("input.txt", "utf-8")).trim();
-    
+
     console.log(solvePuzzle(input));
   } catch (err) {
     console.error(err);
@@ -16,4 +59,3 @@ const main = async () => {
 };
 
 main();
-
