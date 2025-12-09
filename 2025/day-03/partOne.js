f66eb3824639484339777add4eddf165c5405a76
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2025-12-09 15:04:59 +0800

    2025 day 3 part 1

--- 2025/day-03/partOne.js
+++ 2025/day-03/partOne.js
@@ -1,14 +1,45 @@
-  
 import { readFile } from "fs/promises";
 
-const parseInput = (input) => {};
+const parseInput = (input) => {
+  return input.trim().split("\n");
+};
 
-const solvePuzzle = (input) => {};
+const largestJoltage = (bank) => {
+  let best = 0;
+
+  for (let i = 0; i < bank.length; i++) {
+    const a = Number(bank[i]);
+
+    for (let j = i + 1; j < bank.length; j++) {
+      const b = Number(bank[j]);
+
+      const joltage = Number(bank[i] + bank[j]);
+
+      if (joltage > best) {
+        best = joltage;
+      }
+    }
+  }
+
+  return best;
+};
+
+const solvePuzzle = (input) => {
+  const banks = parseInput(input);
+
+  let sum = 0;
+
+  for (const bank of banks) {
+    sum += largestJoltage(bank);
+  }
+
+  return sum;
+};
 
 const main = async () => {
   try {
     const input = (await readFile("input.txt", "utf-8")).trim();
-    
+
     console.log(solvePuzzle(input));
   } catch (err) {
     console.error(err);
@@ -16,4 +47,3 @@ const main = async () => {
 };
 
 main();
-
