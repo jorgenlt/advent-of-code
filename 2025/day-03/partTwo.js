8bcc99aaef472f38c8229dff6d0bbaf4a374a7e7
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2025-12-10 13:49:51 +0800

    2025 day 3 part 2

--- 2025/day-03/partTwo.js
+++ 2025/day-03/partTwo.js
@@ -1,14 +1,50 @@
-  
 import { readFile } from "fs/promises";
 
-const parseInput = (input) => {};
+const parseInput = (input) => {
+  return input.trim().split("\n");
+};
 
-const solvePuzzle = (input) => {};
+const maxSubsequenceOfLength = (s, k) => {
+  const stack = [];
+  const n = s.length;
+
+  for (let i = 0; i < n; i++) {
+    const ch = s[i];
+
+    while (
+      stack.length > 0 &&
+      stack[stack.length - 1] < ch &&
+      stack.length - 1 + (n - i) >= k
+    ) {
+      stack.pop();
+    }
+
+    if (stack.length < k) {
+      stack.push(ch);
+    }
+  }
+
+  return stack.join("");
+};
+
+const solvePuzzle = (input) => {
+  const banks = parseInput(input);
+
+  let sum = 0;
+  const k = 12;
+
+  for (const bank of banks) {
+    const subseq = maxSubsequenceOfLength(bank, k);
+    sum += Number(subseq);
+  }
+
+  return sum;
+};
 
 const main = async () => {
   try {
     const input = (await readFile("input.txt", "utf-8")).trim();
-    
+
     console.log(solvePuzzle(input));
   } catch (err) {
     console.error(err);
@@ -16,4 +52,3 @@ const main = async () => {
 };
 
 main();
-
